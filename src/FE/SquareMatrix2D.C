41a2989cdc5a30d6e44e8ec178ff826163d27961
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2015-06-05 14:29:32 +0200

    implemented some linear algebra for SquareMatrix2D class

--- src/FE/SquareMatrix2D.C
+++ src/FE/SquareMatrix2D.C
@@ -13,6 +13,7 @@
 
 #include <SquareMatrix2D.h>
 #include <string.h>
+#include <LinAlg.h>
 
 TSquareMatrix2D::TSquareMatrix2D(TSquareStructure2D *squarestructure)
   : TSquareMatrix(squarestructure), structure(squarestructure)
@@ -38,6 +39,56 @@ TSquareMatrix2D::~TSquareMatrix2D()
 }
 
 
+void TSquareMatrix2D::reset_non_active()
+{
+  int n_active_rows = this->structure->GetFESpace()->GetN_ActiveDegrees();
+  int * rowPtr = this->structure->GetRowPtr();
+  int index_nonactive = rowPtr[n_active_rows];
+  int n_nonactive_entries = rowPtr[structure->GetN_Rows()] - index_nonactive;
+  memset(Entries + index_nonactive, 0.0, n_nonactive_entries * SizeOfDouble);
+}
+
+void TSquareMatrix2D::reset_active()
+{
+  int n_active_rows = this->structure->GetFESpace()->GetN_ActiveDegrees();
+  int * rowPtr = this->structure->GetRowPtr();
+  // numer of entries in active rows
+  int n_active = rowPtr[n_active_rows];
+  memset(this->Entries, 0.0, n_active * SizeOfDouble);
+}
+
+void TSquareMatrix2D::scale_active(double factor)
+{
+  if(factor == 1.0)
+    return; // no scaling
+  if(factor == 0.0)
+    this->reset_active();
+  
+  // number of active rows
+  int n_active_rows = this->structure->GetFESpace()->GetN_ActiveDegrees();
+  int * rowPtr = this->structure->GetRowPtr();
+  // numer of entries in active rows
+  int n_active = rowPtr[n_active_rows];
+  Dscal(n_active, factor, this->Entries);
+}
+
+void TSquareMatrix2D::add_active(const TSquareMatrix2D& m, double factor)
+{
+  if(this->structure != m.GetStructure() // compare pointers
+     && (*(this->structure)) != (*(m.GetStructure()))) // compare objects
+  {
+    ErrMsg("TMatrix::add : the two matrices do not match.");
+    throw("TMatrix::add : the two matrices do not match.");
+  }
+  
+  // number of active rows
+  int n_active_rows = this->structure->GetFESpace()->GetN_ActiveDegrees();
+  int * rowPtr = this->structure->GetRowPtr();
+  // numer of entries in active rows
+  int n_active = rowPtr[n_active_rows];
+  Daxpy(n_active, factor, m.GetEntries(), this->Entries);
+}
+
 TSquareMatrix2D& TSquareMatrix2D::operator=(const TSquareMatrix2D& rhs)
 {
   // compare structures (first pointers, then structures themselves)
@@ -61,30 +112,15 @@ TSquareMatrix2D& TSquareMatrix2D::operator=(const TSquareMatrix2D& rhs)
 
 TSquareMatrix2D& TSquareMatrix2D::operator*=(double alpha)
 {
-  int nDOFNonActive = structure->GetN_Rows() - structure->GetActiveBound();
-  for (int i=0; i<structure->GetN_Entries() - nDOFNonActive; i++){
-      Entries[i] *= alpha;
-  }
+  this->scale_active(alpha);
   return *this;
 }
 
 
-TSquareMatrix2D& TSquareMatrix2D::operator+=(TSquareMatrix2D & rhsMat)
+TSquareMatrix2D& TSquareMatrix2D::operator+=(const TSquareMatrix2D & rhsMat)
 {
 
-  if(*structure != *rhsMat.structure)
-  {
-    OutPut("ERROR: TSquareMatrix2D::operator+=() The two arguments "
-           << "have different structures. Exiting" << endl);
-    exit(1);
-  }
-  //OutPut(" TSquareMatrix2D::operator+= \n");
-  double *rhsEntries = rhsMat.Entries;
-  
-  int nDOFNonActive = structure->GetN_Rows() - structure->GetActiveBound();
-  for (int i=0; i<structure->GetN_Entries() - nDOFNonActive; i++){
-      Entries[i] += rhsEntries[i];
-  }
+  this->add_active(rhsMat, 1.0);
   return *this;
 }
 
