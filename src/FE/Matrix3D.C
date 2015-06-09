c0f5a70da47eb3e725eb23c5c04b7e6716906f74
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2015-06-09 17:29:17 +0200

    implemented linear algebra routines for TMatrix3D

--- src/FE/Matrix3D.C
+++ src/FE/Matrix3D.C
@@ -13,6 +13,7 @@
 
 #include <Matrix3D.h>
 #include <string.h>
+#include <LinAlg.h>
 
 TMatrix3D::TMatrix3D(TStructure3D *structure)
  : TMatrix(structure)
@@ -34,4 +35,55 @@ void TMatrix3D::resetNonActive()
   int n_nonactive_entries = rowPtr[this->structure->GetN_Rows()]
                            - index_nonactive;
   memset(Entries + index_nonactive, 0.0, n_nonactive_entries * SizeOfDouble);
+}
+
+
+void TMatrix3D::reset_non_active()
+{
+  int n_active_rows = this->structure->GetTestSpace()->GetActiveBound();
+  int * rowPtr = this->structure->GetRowPtr();
+  int index_nonactive = rowPtr[n_active_rows];
+  int n_nonactive_entries = rowPtr[structure->GetN_Rows()] - index_nonactive;
+  memset(Entries + index_nonactive, 0.0, n_nonactive_entries * SizeOfDouble);
+}
+
+void TMatrix3D::reset_active()
+{
+  int n_active_rows = this->structure->GetTestSpace()->GetActiveBound();
+  int * rowPtr = this->structure->GetRowPtr();
+  // number of entries in active rows
+  int n_active = rowPtr[n_active_rows];
+  memset(this->Entries, 0.0, n_active * SizeOfDouble);
+}
+
+void TMatrix3D::scale_active(double factor)
+{
+  if(factor == 1.0)
+    return; // no scaling
+  if(factor == 0.0)
+    this->reset_active();
+  
+  // number of active rows
+  int n_active_rows = this->structure->GetTestSpace()->GetActiveBound();
+  int * rowPtr = this->structure->GetRowPtr();
+  // number of entries in active rows
+  int n_active = rowPtr[n_active_rows];
+  Dscal(n_active, factor, this->Entries);
+}
+
+void TMatrix3D::add_active(const TMatrix3D& m, double factor)
+{
+  if(this->structure != m.GetStructure() // compare pointers
+     && (*(this->structure)) != (*(m.GetStructure()))) // compare objects
+  {
+    ErrMsg("TMatrix::add : the two matrices do not match.");
+    throw("TMatrix::add : the two matrices do not match.");
+  }
+  
+  // number of active rows
+  int n_active_rows = this->structure->GetTestSpace()->GetActiveBound();
+  int * rowPtr = this->structure->GetRowPtr();
+  // number of entries in active rows
+  int n_active = rowPtr[n_active_rows];
+  Daxpy(n_active, factor, m.GetEntries(), this->Entries);
 }
