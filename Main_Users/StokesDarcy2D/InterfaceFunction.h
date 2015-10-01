d913f8e39ad20b36d285d815794026c14dfd045c
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2015-10-01 15:49:07 +0200

    starting stokes_darcy branch
    this is taken from MooNMD and ported to ParMooN. It is not yet completely
    tested.

---
+++ Main_Users/StokesDarcy2D/InterfaceFunction.h
@@ -0,0 +1,212 @@
+#ifndef INTERFACEFUNCTION_H
+#define INTERFACEFUNCTION_H
+class InterfaceFunction;
+
+#include <BlockVector.h>
+#include <auxiliaryFunctions.h>
+#include <StokesProblem.h>
+#include <DarcyProblem.h>
+#include <StokesDarcy2D.h>
+#include <vector>
+
+/** ************************************************************************* */
+/** Description of the class InterfacFunction
+ * An object of this class is a representation of a function on the interface.
+ * This is more or less only implemented in the special way which is needed 
+ * here. We can construct such an interface function using a vector of 
+ * interface edges (vector<TInnerInterfaceJoint *>). The function then can 
+ * be evaluated at any point (on the interface). Other finite element 
+ * functions on either one subdomain (TFEFunction2D*) can be interpolated 
+ * into an interface function. 
+ */
+class InterfaceFunction : public BlockVector
+{
+ private:
+  std::vector<TInnerInterfaceJoint *> interface;
+  /* code for the used space on the interface
+   * 2 : continuous and piecewise quadratic
+   * 1 : continuous and piecewise linear   (not implemented, maybe never will)
+   * 0 : piecewise constant                (not implemented, maybe never will)
+   * -1: piecewise linear                  (not implemented, maybe never will)
+   * -2: piecewise quadratic */
+  int spaceType;
+  /* array determining the degrees of freedom for each edge, i.e. the positions
+   * in the array 'blocks[0]' (see BlockVector.h) which corresponds to this 
+   * edge. It has length 3*interface.size() for (continuous) piecewise 
+   * quadratics. I.e. DOF[3*i+j] tells you the position of the j-th (0<=j<=2) 
+   * degree of freedom on the i-th (0<=i<=interface.size()) edge within the 
+   * array 'blocks[0]'. */
+  std::vector<unsigned int> DOF;
+  /* store information on the neighboring structure. For edge i (interface[i]) 
+   * its neighbors have indices adjacency[2*i] and adjacency[2*i+1]. We call 
+   * the neighbors first and second neighbor. A value of i for the i-th edge 
+   * means there is no neighbor. The first neightbor (At adjacency[2*i]) is at 
+   * (x,y), where (x,y) are from interface[i]->GetParams(x,y,vecx,vecy); Then 
+   * the second neighbor (at adjacency[2*i+1]) is at the other edge point 
+   * (x+vecx,y+vecy). */
+  std::vector<unsigned int> adjacency;
+  /* store normal and tangential vector for each dof.  
+   */
+  std::vector<n_t_vector> normal_tangential;
+  /* depending on which space this functions is supposed to belong to, this
+   * vector contains the dofs which are set to 0. These are dofs at the 
+   * (Dirichlet) boundary. This is needed to restrict this interface function
+   * to the correct interface space (depending on StokesFirst) in case of the
+   * Stecklov-Poincare interface equation
+   */
+  std::vector<unsigned int> *dofs_to_be_restricted;
+  
+  /* create the arrays 'DOF', 'adjacency', 'normal_tangential' and return 
+   * the length of this vector
+   */
+  unsigned int make_adjacency_and_DOF();
+ public:
+  /* constructor */
+  InterfaceFunction(const std::vector<TInnerInterfaceJoint *>& in, int s);
+  
+  // empty constructor, used for iterative template solvers
+  // The InterfaceFunction is not useful after this constructor alone!
+  InterfaceFunction();
+  
+  // copy constructor, just copy all entries
+  InterfaceFunction(const InterfaceFunction& eta);
+  
+  // constructor, given a certain length (needed for gmres)
+  InterfaceFunction(const unsigned int a);
+  
+  /* destructor */
+  ~InterfaceFunction();
+  
+  /* get the function value at a specified point (x,y), terminates the program
+   * if the point is not on the interface. */
+  double GetValue(const double x, const double y) const;
+  /* get the function value at a specified point (x,y) which is on the i-th 
+   * interface edge, throws 'false' if point is not on this edge */
+  double GetValueLocal(const int i, const double x, const double y) const;
+  /* interpolate a function on the interface and add it to this interface
+   * function. The function lives on either one of the subdomains. The 
+   * parameter 'comp' determines if the function or certain derivatives 
+   * are taken into account. The following values are poosible:
+   * 0: value of function 'f' at interface   (default)
+   * 1: normal component of gradient of function 'f' at interface
+   * 2: tangential component of gradient of function 'f' at interface
+   * 3: first component of gradient of function 'f' at interface
+   * 4: second component of gradient of function 'f' at interface
+   *-1: for Raviart-Thomas elements, normal component of f 
+   * 
+   * The additional factor is optional.
+   * */
+  void add(TFEFunction2D *f, int comp=0, double factor=1.0);
+  /* interpolate a vector function on the interface and add it to this 
+   * interface function. The function lives on either one of the subdomains. 
+   * Which component of the vector is used, can be controlled via the 
+   * (optional) parameter 'comp'. The following values are possible:
+   * 1: normal component        (default)
+   * 2: tangential component
+   * 3: first component
+   * 4: second component
+   * 5: n.DD(v).n
+   * 
+   * The additional factor is optional.
+   */
+  void add(TFEVectFunct2D* v, int comp=1, double factor=1.0);
+  /* add other interface function to this one (multiplied by a factor).  
+   * It is checked if 'this' and eta belong to the same space.
+   * The additional factor is optional.
+   * 
+   * "this += factor*eta"
+   */
+  void add(const InterfaceFunction * const eta, double factor=1.0);
+  /* return the normal and tangential vector for a given dof */
+  n_t_vector get_normal(unsigned int dof) const
+  { return normal_tangential[dof]; }
+  
+  /* set this function to zero on the boundary of the interface */
+  void zeroAtBoundary();
+  // restrict correctly according to StokesFirst, see the explanation of the 
+  // other restrict method (a template version) for what 'invert' does
+  void restrict(const StokesDarcy2D& sd, bool invert = false);
+  // restrict to the subspace which is a trace space of the 'subproblem' (either
+  // (StokesProblem or DarcyProblem). This replaces the dof values with zeros,
+  // who touch a Dirichlet (outer) boundary in the respective subdomain.  
+  // If the boolean 'invert' is true, then all values are set to zero, except
+  // those dofs touching a Dirichlet boundary.
+  // this function uses the vector 'dofs_to_be_restricted' which is created in
+  // case it doesn't exist yet. Once it exist, you can use restrict() without 
+  // parameters as well
+  template < class subproblem >
+  void restrict(const subproblem&, bool invert = false);
+  void restrict(bool invert = false);
+  
+  // compute the integral of this InterfaceFunction
+  double integral() const;
+  
+  // Add an appropriate function on the interface to this InterfaceFunction such
+  // that the resulting function has integral 'a'. This makes sure that the
+  // subproblems are solvable, i.e. that the divergence constraint is not 
+  // violated.
+  void set_integral(double a);
+  
+  /* update eta_p
+   * Depending on the updating strategy (e.g. C-RR, D-RR, Neumann-Neumann) the 
+   * update of the interface function is done. Then the interface function is
+   * ready to be passed over to the Darcy part as boundary data.
+   */
+  void update(StokesProblem &s, DarcyProblem &d,InterfaceFunction *eta_f=NULL);
+  /* update  eta_f
+   * Depending on the updating strategy (e.g. C-RR, D-RR, Neumann-Neumann) the 
+   * update of the interface function is done. Then the interface function is
+   * ready to be passed over to the Stokes part as boundary data.
+   */
+  void update(DarcyProblem &d, StokesProblem &s,InterfaceFunction *eta_p=NULL);
+  
+  /* Print out all arrays, for testing */
+  void PrintInfo(std::string name = "") const;
+  /* Print out all x,y,f(x,y) triples */
+  void PrintVals(std::string name = "") const;
+  /*
+   * This is not implemented yet
+   * Write a gnuplot file "a.gp". Type "gnuplot a.gp" so see the output. This 
+   * interface function is plotted against the parameter t in [0,1] which 
+   * parametrises the interface.   
+   */
+  void PrintGnuplotFile(char* a) const;
+  
+  int getSpaceType() const { return spaceType; }
+  
+  bool isBoundaryEdge(unsigned int i) const
+  { return (adjacency[2*i]==i || adjacency[2*i+1]==i); }
+  
+  /* get the DOF of the i-th local dof on 'edge' */
+  int getDOF(const int edge, const int i) const { return DOF.at(3*edge + i); }
+  
+  // operator= more or less only calls the BlockVector-version of operator=
+  InterfaceFunction& operator=(const InterfaceFunction& r)
+  {
+    this->BlockVector::operator=(r);
+    if(spaceType == -4711) 
+    {
+      // empty constructor has been called on this object --> copy all variables
+      // or constructor InterfaceFunction(const double a)
+      spaceType = r.getSpaceType();
+      interface = r.interface;
+      DOF = r.DOF;
+      adjacency = r.adjacency;
+      if(r.dofs_to_be_restricted != NULL)
+      {
+        dofs_to_be_restricted = new std::vector<unsigned int>();
+        *dofs_to_be_restricted = *(r.dofs_to_be_restricted);
+      }
+      else
+        dofs_to_be_restricted = NULL;
+    }
+    return *this;
+  }
+  InterfaceFunction& operator=(const double& a)
+  {
+    this->BlockVector::operator=(a);
+    return *this;
+  }
+};
+
+#endif // INTERFACEFUNCTION_H
---
+++ Main_Users/StokesDarcy2D/main.C
@@ -0,0 +1,307 @@
+#include <auxiliaryFunctions.h>
+#include <StokesProblem.h>
+#include <DarcyProblem.h>
+#include <StokesDarcy2D.h>
+#include <InterfaceFunction.h>
+#include <Constants.h>
+#include <FEDatabase2D.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/** ************************************************************************ */
+int main(int argc, char* argv[])
+{
+  {
+  double time = GetTime();
+  TDomain Domain;
+  TDatabase Database;
+  TFEDatabase2D FEDatabase;
+  
+  /* ==========================================================================
+   * read parameter file
+   * ========================================================================*/
+  Domain.ReadParam(argv[1]);
+  OpenFiles();
+  OutFile.setf(std::ios::scientific);
+  
+  // ==========================================================================
+  // take example from database (input file), see TDatabase::ParamDB->EXAMPLE
+  // during the constructor of the example, some values in the database might be
+  // changed
+  Example_StokesDarcy2D example;
+  // write all database parameters to outfile
+  Database.WriteParamDB(argv[0]);
+  
+  // create output directory, if not already existing
+  if(TDatabase::ParamDB->WRITE_VTK)
+    mkdir(TDatabase::ParamDB->OUTPUTDIR, 0777);
+  
+  // =======================  Parameters ======================================
+  check_all_parameters();
+  // Neumann_Neumann, Robin_Robin, Dirichlet_Dirichlet, 
+  // Neumann_Dirichlet, Dirichlet_Neumann
+  const ProblemType pt = (ProblemType) TDatabase::ParamDB->StoDa_problemType;
+  const int solution_strategy = TDatabase::ParamDB->StoDa_solutionStrategy;
+  const int updating_strategy = TDatabase::ParamDB->StoDa_updatingStrategy;
+  // use Raviart-Thomas or Brezzi-Douglas-Marini elements for Darcy part
+  bool mixed = false;
+  if(pt == Neumann_Dirichlet || pt == Dirichlet_Neumann) 
+    mixed = true; // RT for Robin-Robin not yet possible
+  // type of interface conditions for tangential Stokes velocity: 
+  //  0 : u.t + alpha t.T.n = 0  // Beavers--Joseph--Saffman
+  //  1 : u.t = 0                // zero tangential Stokes velocity
+  const int icond = TDatabase::ParamDB->StoDa_interfaceType;
+  const bool D_RR = (pt == Robin_Robin && updating_strategy == 4);
+  const bool C_RR = (pt == Neumann_Neumann && updating_strategy == 3
+                    && (solution_strategy == 1 || solution_strategy == -1)); 
+  
+  
+  /* ==========================================================================
+   * read boundary parameterization, initialize coarse grid, Refine,
+   * ========================================================================*/
+  OutPut("Initialize domain\n");
+  Domain.Init(TDatabase::ParamDB->BNDFILE, TDatabase::ParamDB->GEOFILE);
+  // vector containing all interface edges (joints)
+  std::vector<TInnerInterfaceJoint *> interface;
+  GetInnerInterfaceJoints(interface, Domain); // fill vector 'interface'
+  // refine up to user defined coarsest level
+  for(int i = 0; i < TDatabase::ParamDB->SC_COARSEST_LEVEL_SCALAR; i++)
+  {
+    Domain.RegRefineAll();
+    GetInnerInterfaceJoints(interface, Domain); // update interface vector
+  }
+  /* ==========================================================================
+   * get the collections of the two subdomains
+   * ========================================================================*/
+  OutPut("\nSome info on the subdomains\n");
+  {
+    // divide the collection
+    TCollection *coll_Darcy = Domain.GetCollection(It_Finest, 0, 1);
+    TCollection *coll_NSE = Domain.GetCollection(It_Finest, 0, 2);
+    
+    // write out some information on the grid.
+    OutPut(" number of all cells       " << coll_NSE->GetN_Cells() 
+                                          + coll_Darcy->GetN_Cells() << endl);
+    OutPut(" number of Stokes cells    " << coll_NSE->GetN_Cells() << endl);
+    OutPut(" number of Darcy cells     " << coll_Darcy->GetN_Cells() << endl);
+    OutPut(" number of interface edges " << interface.size() << endl);
+    // hmin is really the minimum of the largest edge of each cell
+    // hmin is __not__ the smallest edge, in a uniform grid it equals hmax  
+    {
+      double hmin, hmax;
+      coll_Darcy->GetHminHmax(&hmin, &hmax);
+      OutPut(" Darcy  h_min " << hmin << ", hmax " << hmax << endl);
+      coll_NSE->GetHminHmax(&hmin, &hmax);
+      OutPut(" Stokes h_min " << hmin << ", hmax " << hmax << endl);
+    }
+    delete coll_Darcy;
+    delete coll_NSE;
+  }
+  if(TDatabase::ParamDB->WRITE_PS)
+  {
+    // get the total collection on the finest level (both subdomains)
+    TCollection *coll_tot = Domain.GetCollection(It_Finest, 0);
+    // if you want cell numbers drawn in PS file: uncomment the line 104
+    // "#define __GRIDCELL_WITH_NUMBERS__" in src/Geometry/GridCell.C
+    std::ostringstream os(std::ios::trunc);
+    os << TDatabase::ParamDB->BASENAME << "grid.ps" << ends;
+    Domain.PS(os.str().c_str(), coll_tot);
+    delete coll_tot;
+  }
+  OutPut("\n");
+  /* =======================   inteface function   ========================= */
+  // if D-RR, then -2 (discontinuous) otherwise 2 (continuous)
+  int spaceType = D_RR ? -2 : 2;
+  
+  // no discontinuous interface functions needed, because even for D-RR, the 
+  // assembling of the interface terms is done during output of stokes and darcy
+  // classes.
+  //int spaceType = -2;
+  // 'eta' is needed for solution_strategy 2 and 3
+  InterfaceFunction eta(interface, spaceType);
+  OutPut("number of dofs on the interface: " << eta.length() << endl);
+  Out("We use " << (spaceType > 0 ? "" : "dis") << 
+      "continuous interface functions\n", 1);
+  OutPut(endl);
+  
+  /* ==========================================================================
+   * build matrices , assemble all terms (except for the interface integrals)
+   * the matrix does not change during the interface iteration !!
+   * ========================       NSE        ============================= */
+  std::vector<InterfaceCondition> interface_conditions;
+  if(!D_RR && !pt == Robin_Robin)
+    interface_conditions.push_back(Neumann);
+  if(solution_strategy == 3 || solution_strategy == -3) // Stecklov-Poincare
+    interface_conditions.push_back(Dirichlet);
+  if(solution_strategy != 3 && solution_strategy != -3 // Stecklov-Poincare
+       && (D_RR || pt == Robin_Robin || C_RR))
+    interface_conditions.push_back(Robin);
+  //interface_conditions.push_back(weakRobin);
+  //interface_conditions.push_back(DirichletSTAB);
+  
+  std::map<InterfaceCondition, StokesProblem*> ns_problems;
+  Example_NSE2D * ns_example = example.get_stokes_example();
+  for(unsigned int i = 0; i < interface_conditions.size(); i++)
+  {
+    OutPut("intitializing a " << interface_conditions[i] << 
+           " (Navier-) Stokes problem\n");
+    StokesProblem * ns;
+    ns = new StokesProblem(Domain, ns_example, interface_conditions[i],
+                           interface, icond);
+    if(TDatabase::ParamDB->SC_VERBOSE > 0) ns->print_assemble_input_output();
+    ns->assemble();
+    ns->find_global_DOF_interface(eta);
+    ns->mat_NSE_iIntegrals(); // assemble all interface integrals into matrix
+    if(TDatabase::ParamDB->StoDa_periodicBoundary) // change this if necessary)
+    {
+      if(!C_RR) ns->get_matrix().block(8)->add(0, 0, 1e30);
+      if(TDatabase::ParamDB->LAPLACETYPE == 1)
+      {
+        // assemble the additional term (nu (grad u)^T n , v)
+        // for Poisseuille flow this is needed to get the correct solution
+        //map<int, double> comps;
+        //comps.insert(pair<int, double>(3, -1.0));
+        //comps.insert(pair<int, double>(5, -1.0));
+        //ns->assemble_boundary_term("n nu graduT n v", comps);
+        ErrMsg("Stokes_2D::assemble_boundary_term not yet ported from MooNMD");
+        exit(1);
+      }
+      ns->findPeriodicDOFs();
+      ns->makePeriodicBoundary();
+    }
+    ns->reorder_rows();
+    ns->copy_rhs(); // copy non-interface-part of rhs
+    ns->prepare_input_output_maps(eta);
+    if(interface_conditions[i] == Dirichlet
+       && (TDatabase::ParamDB->EXAMPLE == 0
+           || TDatabase::ParamDB->EXAMPLE == 1))
+      ns->get_matrix().block(8)->add(0, 0, 1e30);
+    ns_problems[interface_conditions[i]] = ns;
+  }
+  OutPut(endl);
+  
+  /* ==========================     Darcy     ============================== */
+  // needed for primal Darcy
+  TDatabase::ParamDB->ANSATZ_ORDER = TDatabase::ParamDB->VELOCITY_SPACE;
+  if(mixed)
+    TDatabase::ParamDB->VELOCITY_SPACE = 1002;
+  
+  std::map<InterfaceCondition, DarcyProblem*> d_problems;
+  for(unsigned int i = 0; i < interface_conditions.size(); i++)
+  {
+    OutPut("intitializing a " << (mixed ? "mixed " : "primal ")
+           << interface_conditions[i] << " Darcy problem\n");
+    DarcyProblem * d;
+    d = new DarcyProblem(Domain, &example, interface_conditions[i],
+                         interface, mixed, 1);
+    if(TDatabase::ParamDB->SC_VERBOSE > 0) d->print_assemble_input_output();
+    d->assemble(); // assemble linear terms
+    d->find_global_DOF_interface(eta);
+    d->mat_DARCY_iIntegrals(); // assemble all interface integrals into matrix
+    if(TDatabase::ParamDB->StoDa_periodicBoundary)
+    {
+      d->findPeriodicDOFs();
+      d->makePeriodicBoundary();
+    }
+    d->copy_rhs();
+    d->prepare_input_output_maps(eta); // assemble maps to and from interface
+    d_problems[interface_conditions[i]] = d;
+  }
+  
+  /* ==================   Stokes--Darcy problem class   ==================== */
+  // system, we're trying to solve
+  StokesDarcy2D sd(ns_problems, d_problems);
+  
+  Out(std::setfill('*') << std::setw(79) << "*\n" << std::setfill(' '), 0);
+  Out("Everything has been set up (withing " << GetTime()-time << " seconds), "
+      << "ready to solve\n\n", 0);
+  /* ===================   compute direct solution   ======================= */
+  time = GetTime();
+  if(solution_strategy >= 0)
+  {
+    sd.solveDirect();
+    
+    double error = ErrorOnInterface(*(sd.c_stokes()), *(sd.c_darcy()), -1);
+    OutPut("  Error on interface " << error << endl)
+    WriteVtk_and_measureErrors(*(sd.c_stokes()), *(sd.c_darcy()), NULL, -1);
+    Out("Done with direct solution of the coupled system within " << 
+        GetTime()-time << " seconds\n", 0);
+  }
+  
+  if(TDatabase::ParamDB->SC_VERBOSE) sd.check_equalities();
+  
+  if(solution_strategy != 0) // not only direct solve
+  {
+    Out(std::setfill('*') << std::setw(79) << "*\n" << std::setfill(' '), 0);
+    Out("Starting to solve iteratively\n\n", 0);
+  }
+  
+  /* =======================   interface iteration   ======================= */
+  time = GetTime();
+  if(solution_strategy == 1 || solution_strategy == -1)
+  {
+    // solve iteratively using blockwise Jacobi or blockwise Gauss--Seidel
+    // 'eta_f' and 'eta_p' are needed for solution_strategy 1, they are copies
+    // of 'eta' which iteself is not needed here.
+    InterfaceFunction eta_f(eta);
+    InterfaceFunction eta_p(eta);
+    
+    int maxit = TDatabase::ParamDB->StoDa_nIterations;
+    for(int it = 0; it < maxit; it++)
+    {
+      OutPut("\nInterface Iteration: " << it << endl);
+      OutPut(" Memory: " << setw(8) << GetMemory()/(1048576.0) << " MB\n");
+      sd.solve_coupled_system(eta_f, eta_p);
+      
+      // check stopping criterion, compute errors, make pictures
+      if(sd.stopIteration(it))
+        break;
+    } // end loop for coupling
+    Out("Done with classical iterative method within " << GetTime()-time <<
+        " seconds\n", 0);
+  }
+  else if(solution_strategy == 2 || solution_strategy == -2)
+  {
+    // solve a fixed point formulation
+    int maxit = TDatabase::ParamDB->StoDa_nIterations;
+    for(int it = 0; it < maxit; it++)
+    {
+      OutPut("\nInterface Iteration: " << it << endl);
+      OutPut(" Memory: " << setw(8) << GetMemory()/(1048576.0) << " MB\n");
+      sd.solve_fixed_point(eta);
+      
+      // check stopping criterion, compute errors, make pictures
+      if(sd.stopIteration(it))
+        break;
+    } // end loop for coupling
+    OutPut("norm of final eta " << eta.norm() << endl);
+    Out("Done with fixed point iteration within " << GetTime()-time <<
+         " seconds\n", 0);
+  }
+  else if(solution_strategy == 3 || solution_strategy == -3)
+  {
+    OutPut("\nSolving the Stecklov-Poincare equation\n");
+    // solve a Stecklov-Poincare equation using template iterative solvers
+    sd.solve_Stecklov_Poincare(eta);
+    
+    const double error = ErrorOnInterface(*(sd.stokes()), *(sd.darcy()), 0);
+    OutPut("  Error on interface " << error << endl)
+    WriteVtk_and_measureErrors(*(sd.stokes()), *(sd.darcy()), NULL, 0);
+    Out("Done with Steklov-Poincare iteration within " << GetTime()-time <<
+        " seconds\n", 0);
+  }
+  /* ============================   clean up   ============================= */
+  for(unsigned int i = 0; i < interface_conditions.size(); i++)
+  {
+    delete ns_problems.at(interface_conditions[i]);
+    delete d_problems.at(interface_conditions[i]);
+  }
+  delete ns_example;
+  }
+  OutPut("MEMORY: " << setw(10) << GetMemory()/(1048576.0) << " MB" << endl);
+  OutPut("used time: " << GetTime() << endl);
+  OutPut("Program finished normally\n");
+  CloseFiles();
+  return 0;
+}
