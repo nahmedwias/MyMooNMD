743b3003d3ac0395048f0c0169a2c3d1e29ecee2
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2015-10-07 16:20:37 +0200

    took out AssembleMat3D from compilation
    this needs to be rewritten

--- cmake/sources_system.cmake
+++ cmake/sources_system.cmake
@@ -18,7 +18,6 @@ list(APPEND SYST_SOURCES_2D "${PROJECT_SOURCE_DIR}/src/System/Darcy2D.C")
 list(APPEND SYST_SOURCES_2D "${PROJECT_SOURCE_DIR}/src/System/Time_CD2D.C")
 
 # Source files to be added to the 3D library.
-list(APPEND SYST_SOURCES_3D "${PROJECT_SOURCE_DIR}/src/System/AssembleMat3D.C")
 list(APPEND SYST_SOURCES_3D "${PROJECT_SOURCE_DIR}/src/System/CD3D.C")
 
 
