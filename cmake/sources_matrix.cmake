02a9f051289c4ece8846294afa511051a3add3c4
Clemens Bartsch <bartsch@wias-berlin.de>
2015-10-19 11:31:47 +0200

    Removed erroneous todo in cmake files.

--- cmake/sources_matrix.cmake
+++ cmake/sources_matrix.cmake
@@ -5,6 +5,7 @@
 # 2015/08/25 Ulrich Wilbrandt: Adjust to Clemens changes to the cmake system
 #
 
+# Include header files. 
 include_directories("${CMAKE_SOURCE_DIR}/include/Matrix")
 
 # Source files to be added to the 2D library.
