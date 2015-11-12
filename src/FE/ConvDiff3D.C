3d8175743341fa73095c85ac9411662f8c705446
Clemens Bartsch <bartsch@wias-berlin.de>
2015-11-12 15:57:57 +0100

    Made method collection ConvDiff3D (Desperately needs refactoring) work with LocalAssembling class.

--- src/FE/ConvDiff3D.C
+++ src/FE/ConvDiff3D.C
@@ -119,6 +119,67 @@ double ***LocMatrices, double **LocRhs)
   }                              // endfor i
 }
 
+void BilinearAssembleGalerkin(double Mult, double *coeff, double* param,
+						      double hK, double **OrigValues, int *N_BaseFuncts,
+							  double ***LocMatrices, double **LocRhs)
+{
+  double **Matrix, *Rhs, val, *MatrixRow;
+  double ansatz000, ansatz100, ansatz010, ansatz001;
+  double test000, test100, test010, test001;
+  double *Orig0, *Orig1, *Orig2, *Orig3;
+  int i,j, N_;
+  double c0, c1, c2, c3, c4, c5;
+
+  Matrix = LocMatrices[0];
+  Rhs = LocRhs[0];
+
+  N_ = N_BaseFuncts[0];
+
+  Orig0 = OrigValues[0];
+  Orig1 = OrigValues[1];
+  Orig2 = OrigValues[2];
+  Orig3 = OrigValues[3];
+
+  c0 = coeff[0];
+  c1 = coeff[1];
+  c2 = coeff[2];
+  c3 = coeff[3];
+  c4 = coeff[4];
+  c5 = coeff[5];
+
+
+//   cout << " c0 " << c0 <<" c5 " << c5 << endl;
+
+  for(i=0;i<N_;i++)
+  {
+    MatrixRow = Matrix[i];
+    test100 = Orig0[i];
+    test010 = Orig1[i];
+    test001 = Orig2[i];
+    test000 = Orig3[i];
+
+    Rhs[i] += Mult*test000*c5;
+
+//      if(TDatabase::ParamDB->P14<Mult) TDatabase::ParamDB->P14=Mult;
+
+    for(j=0;j<N_;j++)
+    {
+      ansatz100 = Orig0[j];
+      ansatz010 = Orig1[j];
+      ansatz001 = Orig2[j];
+      ansatz000 = Orig3[j];
+
+      val = c0*(test100*ansatz100+test010*ansatz010+test001*ansatz001);
+      val += (c1*ansatz100+c2*ansatz010+c3*ansatz001)*test000;
+      val += c4*ansatz000*test000;
+
+      val *= Mult;
+
+      MatrixRow[j] += val;
+    }                            // endfor j
+  }                              // endfor i
+}
+
 
 // SDFEM without 2nd derivatives
 void BilinearAssemble_SD(double Mult, double *coeff, double hK,
