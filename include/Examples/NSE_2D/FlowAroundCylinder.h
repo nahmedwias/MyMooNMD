5dcc0afb78ff3c3afa451e3a5423be3fcd9b16a7
Moritz Hoffmann <clonker@gmail.com>
2015-11-23 21:10:38 +0100

    add flow around cylinder from moonmd

---
+++ include/Examples/NSE_2D/FlowAroundCylinder.h
@@ -0,0 +1,88 @@
+// Navier-Stokes problem, Benchmark problem
+//
+// u(x,y) = unknown
+// p(x,y) = unknown
+
+void ExampleFile() {
+    OutPut("Example: FlowAroundCylinder.h" << endl);
+}
+
+// ========================================================================
+// exact solution
+// ========================================================================
+void ExactU1(double x, double y, double *values) {
+    values[0] = 0;
+    values[1] = 0;
+    values[2] = 0;
+    values[3] = 0;
+}
+
+void ExactU2(double x, double y, double *values) {
+    values[0] = 0;
+    values[1] = 0;
+    values[2] = 0;
+    values[3] = 0;
+}
+
+void ExactP(double x, double y, double *values) {
+    values[0] = 0;
+    values[1] = 0;
+    values[2] = 0;
+    values[3] = 0;
+}
+
+// ========================================================================
+// boundary conditions
+// ========================================================================
+void BoundCondition(int i, double t, BoundCond &cond) {
+    if (i == 1) {
+        cond = NEUMANN;
+        TDatabase::ParamDB->INTERNAL_PROJECT_PRESSURE = 0;
+    } else {
+        cond = DIRICHLET;
+    }
+}
+
+void U1BoundValue(int BdComp, double Param, double &value) {
+    switch (BdComp) {
+        case 0:
+            value = 0;
+            break;
+        case 1:
+            value = 0;
+            break;
+        case 2:
+            value = 0;
+            break;
+        case 3:
+            value = 1.2 * Param * (1 - Param); // 4*0.3
+            break;
+        case 4:
+            value = 0;
+            break;
+        default:
+            cout << "wrong boundary part number: " << BdComp << endl;
+    }
+}
+
+void U2BoundValue(int BdComp, double Param, double &value) {
+    value = 0;
+    if (BdComp > 4) cout << "wrong boundary part number: " << BdComp << endl;
+}
+
+// ========================================================================
+// coefficients for Stokes form: A, B1, B2, f1, f2
+// ========================================================================
+void LinCoeffs(int n_points, double *x, double *y,
+               double **parameters, double **coeffs) {
+    double eps = 1 / TDatabase::ParamDB->RE_NR;
+    double *coeff;
+
+    for (auto i = 0; i < n_points; i++) {
+        coeff = coeffs[i];
+
+        coeff[0] = eps;
+        coeff[1] = 0; // f1
+        coeff[2] = 0; // f2
+    }
+}
