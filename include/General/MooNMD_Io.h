1f5e0c0ee38c352ae7e59103551b89388e44c1ba
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2015-11-25 10:48:15 +0100

    updated the print and printToFile methods
    the functionality is the same, but they are more user friendly, I think
    
    updated the documentation as well

--- include/General/MooNMD_Io.h
+++ include/General/MooNMD_Io.h
@@ -38,7 +38,7 @@ extern std::ofstream& OutFile;
 /**
 @brief everything related to writing to console and files
  
-Especially the methods print and print_file are defined in this namespace. The
+Especially the methods print and printToFile are defined in this namespace. The
 proposed usage within ParMooN is as follows:
 
 At the beginning of any ParMooN program make a call to 
@@ -46,7 +46,7 @@ At the beginning of any ParMooN program make a call to
     Output::set_outfile("test_outfile.txt");
 This will make sure that all calls to print will write its arguments to
 `std::cout` as well as to the file 'test_outfile.txt'. Before calling 
-`Output::set_outfile` the print methods only write to `std::cout`.
+Output::set_outfile the print methods only write to `std::cout`.
 
 Use print with any number of arguments. An additional template parameter 
 (`unsigned int`) is recommended to indicate at which verbosity level you 
@@ -59,28 +59,42 @@ which determines whether or not to print and is in the range of 1 and 5
     print<3>("print only if verbosity level is 3 or larger")
     print<4>("print only if verbosity level is 4 or larger")
 
-Calling `Output::print` without a template argument, is equivalent to calling 
-`Output::print<1>`. This means it will be printed for any verbosity level.
-There is only one exception: If `Output::suppressAll()` is called, then nothing
+Calling Output::print without a template argument, is equivalent to calling 
+Output::print<1>. This means it will be printed for any verbosity level.
+There is only one exception: If Output::suppressAll() is called, then nothing
 is ever written to `std::cout` or the outfile. However in that case it is still
 possible to write to a second outfile through calling redirect.
 
 
-In summary the `Output::print` method writes to
+In summary the Output::print method writes to
 
-- only `std::cout` if `Output::set_outfile` has not yet been called
-  (discouraged)
-- `std::cout` and the outfile if `Output::set_outfile` has been called
+- only `std::cout` if Output::set_outfile has not yet been called (discouraged)
+- `std::cout` and the outfile if Output::set_outfile has been called
   (recommended)
-- only to an extra outfile if `Output::redirect` has been called
-- nowhere if `Output::suppressAll` has been called
+- only to an extra outfile if Output::redirect has been called
+- nowhere if Output::suppressAll has been called
 
 where only in the first two cases the verbosity matters at all.
 
 You can control the verbosity level using the methods 
-`Output::increaseVerbosity()` and `Output::decreaseVerbosity()`.
+Output::setVerbosity(unsigned int i),
+Output::increaseVerbosity() and Output::decreaseVerbosity().
 
-At the end of the program call `Output::close_file()`.
+At the end of the program call Output::close_file().
+
+The user should only call the following methods from this namespace and ignore
+the rest of it:
+
+- Output::increaseVerbosity
+- Output::decreaseVerbosity
+- Output::setVerbosity
+- Output::print<>
+- Output::printToFile<>
+- Output::set_outfile
+- Output::close_file
+- Output::suppressAll
+- Output::redirect
+- Output::resetOutfile
  */
 namespace Output
 {
@@ -93,8 +107,8 @@ namespace Output
   /// @brief suppress all output to std::cout and the outfile
   ///
   /// Only calls to Output::print are suppressed. Direct calls to std::cout are
-  /// still possible. This lasts until Output::increaseVerbosity is called 
-  /// again.
+  /// still possible. This lasts until Output::increaseVerbosity, 
+  /// Output::decreaseVerbosity, or Output::setVerbosity is called again.
   void suppressAll();
   
   /// @brief set the outfile
@@ -108,18 +122,20 @@ namespace Output
   /// Files can be opened through Output::set_outfile and Output::redirect.
   void close_file();
   
-  /// \brief redirect all output to this file, nowhere else
-  ///
-  /// This method opens the file and all subsequent calls to print and std::cout
-  /// will write into this file (no matter what the verbosity template parameter
-  /// is set to). To return to the normal behavior of the print methods call
-  /// Output::resetOutfile(). Typically this is used to write all output of a
-  /// block of code into a separate file:
-  ///     Output::redirect("file_for_output_of_f");
-  ///     f(); // call some function which calls print a lot
-  ///     Output::resetOutfile();
-  ///
-  /// \note Using `printf` will still give you output on the console
+  /** \brief redirect all output to this file, nowhere else
+   *
+   * This method opens the file and all subsequent calls to print and std::cout
+   * will write into this file (no matter what the verbosity template parameter
+   * is set to). To return to the normal behavior of the print methods call
+   * Output::resetOutfile(). Typically this is used to write all output of a
+   * block of code into a separate file:
+   * \code{.cpp}
+   *      Output::redirect("file_for_output_of_f");
+   *      f(); // call some function which calls print a lot
+   *      Output::resetOutfile();
+   * \endcode
+   * \note Using `printf` will still give you output on the console
+   */
   void redirect(std::string filename);
   /// @brief close the file after a call to Output::redirect.
   ///
@@ -129,39 +145,29 @@ namespace Output
   
   /// @brief Write something to std::cout and the outfile 
   ///
-  /// This is printed only if the template parameter verbosity is not too small.
+  /// This is printed only if the template parameter verbosity is not too large.
   /// That means verbosity 1 (the default) will always be printed, while with 
   /// larger numbers it is only printed if the verbosity level is high enough.
   /// Note that verbosity must be within 1 and 5 (maxVerbosity), other values
   /// (including 0) give a compile error. 
   /// 
-  /// The second template parameter T can be any type which can be inserted into
-  /// a std::ostream via '<<'.
-  template<unsigned int verbosity = 1, typename T>
-  void print(T const& t);
-  
-  /// @brief variadic template to enable variable number of arguments to print
-  /// 
-  /// This method recursively calls the itself until only two template 
-  /// parameters are left, then it calls the other print method. For more 
-  /// documentation, see there.
-  template<unsigned int verbosity = 1, typename First, typename ... Rest>
-  void print(First const& first, Rest const& ... rest);
-  
+  /// The template parameters `Arguments` can be of any type which can be 
+  /// inserted into a std::ostream via `<<`.
+  ///
+  /// This is a variadic template to enable variable number of arguments to 
+  /// print.
+  template<unsigned int verbosity = 1, typename ... Arguments>
+  void print(Arguments const& ... rest);
   
   /// @brief print only to the outfile depending on verbosity
   ///
   /// This method behaves like the corresponding print, but does not print to
   /// std::cout, only to the outfile.
-  template<unsigned int verbosity = 1, typename T>
-  void printToFile(T const& t);
-  /// @brief variadic template to enable variable number of arguments to print
-  /// only to the outfile depending on verbosity
   ///
-  /// This method behaves like the corresponding print, but does not print to
-  /// std::cout, only to the outfile.
-  template<unsigned int verbosity = 1, typename First, typename ... Rest>
-  void printToFile(First const& first, Rest const& ... rest);
+  /// This is a variadic template to enable variable number of arguments to 
+  /// print.
+  template<unsigned int verbosity = 1, typename ... Arguments>
+  void printToFile(Arguments const& ... rest);
 };
 
 
@@ -171,40 +177,27 @@ namespace Output
 // have to worry about the above declarations.
 namespace Output
 {
-  // maximum allowed verbosity
+  /// @brief maximum allowed verbosity level
   constexpr unsigned int maxVerbosity = 5;
-  // v should be between 1 and maxVerbosity
+  /// @brief a helper function the user should not call
+  ///
+  /// Return true if is v smaller or equal to the current verbosity level.
+  /// v should be between 1 and maxVerbosity, but this is not enforced here.
   bool verbose(unsigned int v);
   
+  /// @brief a helper function the user should not call
+  ///
+  /// Returns true after a call to Output::redirect, otherwise false.
   bool writeOnlyToFile();
+  /// @brief a helper function the user should not call
+  /// 
+  /// Get the stream of the outfile. If Output::redirect has been called this 
+  /// returns the stream of that file.
   std::ofstream& get_outfile();
   
-  // usual print to print to cout and file depending on verbosity
-  template<unsigned int verbosity, typename T>
-  void print(T const& t)
-  {
-    static_assert(verbosity > 0,
-                  "It makes no sense to call print with 0 verbosity. "
-                  "Use a value greater than 0");
-    static_assert(verbosity <= maxVerbosity, 
-                  "calling Output::print with verbosity too large");
-    
-    if(!writeOnlyToFile())
-    {
-      if(verbose(verbosity))
-      {
-        std::cout << t << std::endl;
-        get_outfile() << t << std::endl;
-      }
-    }
-    else
-    {
-      get_outfile() << t << std::endl;
-    }
-  }
-  
-  template<unsigned int verbosity, typename First, typename ... Rest>
-  void print(First const& first, Rest const& ... rest)
+  // implementation of the print method
+  template<unsigned int verbosity, typename ... Arguments>
+  void print(Arguments const& ... args)
   {
     static_assert(verbosity > 0,
                   "It makes no sense to call print with 0 verbosity"
@@ -212,54 +205,40 @@ namespace Output
     static_assert(verbosity <= maxVerbosity, 
                   "calling Output::print with verbosity too large");
     
-    if(!writeOnlyToFile())
+    // the following writes all the arguments into the stream. With c++17 we can
+    // also use 'fold expressions':
+    // (std::cout << ... << args) << std::endl;
+    // (get_outfile() << ... << args) << std::endl;
+    // http://stackoverflow.com/a/21812549
+    // http://stackoverflow.com/a/33621132
+    std::ostringstream stream;
+    using List= int[];
+    (void)List{0, ( (void)(stream << args), 0 ) ... };
+    
+    if(!writeOnlyToFile() && verbose(verbosity))
     {
-      if(verbose(verbosity))
-      {
-        std::cout << first;
-        get_outfile() << first;
-        Output::print<verbosity>(rest ...);
-      }
+      std::cout << stream.str() << std::endl; 
     }
-    else
-    {
-      get_outfile() << first;
-      Output::print<verbosity>(rest ...);
-    }
+    get_outfile() << stream.str() << std::endl;
   }
   
-  
-  // print only to file depending on verbosity
-  template<unsigned int verbosity, typename T>
-  void printToFile(T const& t)
+  // implementation of the printToFile method
+  template<unsigned int verbosity, typename ... Arguments>
+  void printToFile(Arguments const& ... args)
   {
     static_assert(verbosity > 0,
-                  "It makes no sense to call printToFile with 0 verbosity"
+                  "It makes no sense to call print with 0 verbosity"
                   "Use a value greater than 0");
     static_assert(verbosity <= maxVerbosity, 
-                  "calling Output::printToFile with verbosity too large");
+                  "calling Output::print with verbosity too large");
     
     if(verbose(verbosity))
     {
-      get_outfile() << t << std::endl;
+      using List= int[];
+      (void)List{0, ( (void)(get_outfile() << args), 0 ) ... };
+      get_outfile() << std::endl;
     }
   }
-  
-  template<unsigned int verbosity, typename First, typename ... Rest>
-  void printToFile(First const& first, Rest const& ... rest)
-  {
-    static_assert(verbosity > 0,
-                  "It makes no sense to call printToFile with 0 verbosity"
-                  "Use a value greater than 0");
-    static_assert(verbosity <= maxVerbosity, 
-                  "calling Output::printToFile with verbosity too large");
-    
-    if(verbose(verbosity))
-    {
-      get_outfile() << first;
-      Output::printToFile<verbosity>(rest ...);      
-    }
-  }  
 };
 
 // old macro to print things into a file and t std::cout, deprecated, use 
