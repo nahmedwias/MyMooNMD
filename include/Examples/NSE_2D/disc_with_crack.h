00095fd674b3aee697bfe74bb6b15c532f9692f6
Moritz Hoffmann <clonker@gmail.com>
2015-12-26 22:26:53 +0100

    derivatives for disc with crack

--- include/Examples/NSE_2D/disc_with_crack.h
+++ include/Examples/NSE_2D/disc_with_crack.h
@@ -6,26 +6,38 @@ void ExactU1(double x, double y, double *values) {
     double r = sqrt(x * x + y * y);
     double theta = atan(y / x);
     values[0] = (3 * sqrt(r) / 2) * (cos(theta / 2) - cos(3 * theta / 2));
-    values[1] = 3 * sin(theta / 2) * (-x * y * (sqrt((x * x) / (y * y) + 1) + 2)) / (2 * r * r * r * r * sqrt(r));
-    values[2] = 3 * sin(3 * theta / 2) * (y * y + 3 * x * x + x * r) / (2 * r * r * r * r * sqrt(r));
-    values[3] = 0;
+    values[1] = (x / (2. * r * sqrt(r))) * (cos(theta / 2) - cos(3 * theta / 2)) + (y * sqrt(r) / (2 * r * r)) * (sin(theta / 2) - 3 * sin(3 * theta / 2));
+    values[2] = (y / (2. * r * sqrt(r))) * (cos(theta / 2) - cos(3 * theta / 2)) + (x * sqrt(r) / (2 * r * r)) * (3 * sin(3 * theta / 2) - sin(theta / 2));
+    double u1_xx = (x * y / (2 * r * r * r * sqrt(r))) * (sin(theta / 2) - 3 * sin(3 * theta / 2)) + ((1 / (2 * r * sqrt(r))) - 3 * x * x / (4 * r * r * r * sqrt(r))) * (cos(theta / 2) - cos(3 * theta / 2))
+                   + sqrt(r) * ((y * y * y * sin(theta / 2) / (x * r * r * r * r)) - (3 * y * y * y * sin(3 * theta / 2) / (x * r * r * r * r)) - (y * y * cos(theta / 2) / (4 * r * r * r * r)) +
+                                (9 * y * y * cos(3 * theta / 2) / (4 * r * r * r * r)) - (y * sin(theta / 2) / (x * r * r)) + (3 * y * sin(3 * theta / 2) / (x * r * r)));
+    double u1_yy = (x * y / (2 * r * r * r * sqrt(r))) * (3 * sin(3 * theta / 2) - sin(theta / 2)) + ((1 / (2 * r * sqrt(r))) - 3 * x * x / (4 * r * r * r * sqrt(r))) * (cos(theta / 2) - cos(3 * theta / 2))
+                   + sqrt(r) * ((-x * x * cos(theta / 2) / (4 * r * r * r * r)) + (9 * x * x * cos(3 * theta / 2) / (4 * r * r * r * r)) + (x * y * sin(theta / 2) / (r * r * r * r)) - (3 * x * y * sin(3 * theta / 2) / (r * r * r * r)));
+    values[3] = u1_xx + u1_yy;
 }
 
 void ExactU2(double x, double y, double *values) {
     double r = sqrt(x * x + y * y);
     double theta = atan(y / x);
-    values[0] = (3 * sqrt(r) / 2) * (3 * sin(theta / 2) - sin(3 * theta / 2));
-    values[1] = 0;
-    values[2] = 0;
-    values[3] = 0;
+    values[0] = sqrt(r) * (3 * sin(theta / 2) - sin(3 * theta / 2));
+    values[1] = (x / (2 * r * sqrt(r))) * (3 * sin(theta / 2) - sin(3 * theta / 2)) + (3 * y * sqrt(r) / (2 * r * r)) * (cos(3 * theta / 2) - cos(theta / 2));
+    values[2] = (y / (2 * r * sqrt(r))) * (3 * sin(theta / 2) - sin(3 * theta / 2)) + (3 * x * sqrt(r) / (2 * r * r)) * (cos(theta / 2) - cos(3 * theta / 2));
+    double u2_xx = (1 / (2 * r * sqrt(r)) - 3 * x * x / (4 * r * r * r * sqrt(r))) * (3 * sin(theta / 2) - sin(3 * theta / 2)) + (3 * x * y / (2 * r * r * r * sqrt(r))) * (cos(theta / 2) - cos(3 * theta / 2))
+                   + 3 * sqrt(r) *
+                     ((y * y * y * cos(3 * theta / 2) / (x * r * r * r * r)) + (3 * y * y * sin(3 * theta / 2) / (4 * r * r * r * r)) - (y * cos(3 * theta / 2) / (x * r * r)) - (y * y * y * cos(theta / 2) / (x * r * r * r * r)) -
+                      (y * y * sin(theta / 2) / (4 * r * r * r * r)) + (y * cos(theta / 2) / (x * r * r)));
+    double u2_yy = (1 / (2 * r * sqrt(r)) - 3 * y * y / (4 * r * r * r * sqrt(r))) * (3 * sin(theta / 2) - sin(3 * theta / 2)) + (3 * x * y / (2 * r * r * r * sqrt(r))) * (cos(3 * theta / 2) - cos(theta / 2))
+                   + 3 * sqrt(r) *
+                     ((3 * x * x * sin(3 * theta / 2) / (4 * r * r * r * r)) + (x * y * cos(3 * theta / 2) / (r * r * r * r)) - (3 * x * x * sin(theta / 2) / (4 * r * r * r * r)) - (3 * x * y * cos(theta / 2) / (r * r * r * r)));
+    values[3] = u2_xx + u2_yy;
 }
 
 void ExactP(double x, double y, double *values) {
     double r = sqrt(x * x + y * y);
     double theta = atan(y / x);
-    values[0] = -6 / (sqrt(r)) * cos(theta / 2);
-    values[1] = 0;
-    values[2] = 0;
+    values[0] = (-6 / (sqrt(r))) * cos(theta / 2);
+    values[1] = (3/(r*r*sqrt(r))) * (x*cos(theta/2) - y*sin(theta/2));
+    values[2] = (3/(r*r*sqrt(r))) * (x*sin(theta/2) - y*cos(theta/2));
     values[3] = 0;
 }
 
@@ -41,7 +53,7 @@ void U1BoundValue(int BdComp, double Param, double &value) {
     if (BdComp == 2) {
         // on circle
         value = (cos(Pi * Param) - cos(3. * Pi * Param));
-    } else if(BdComp > 2) {
+    } else if (BdComp > 2) {
         cerr << "wrong boundary part number: " << BdComp << endl;
     }
 }
@@ -51,7 +63,7 @@ void U2BoundValue(int BdComp, double Param, double &value) {
     if (BdComp == 2) {
         // on circle
         value = (3. * sin(Pi * Param) - sin(3. * Pi * Param));
-    } else if(BdComp > 2) {
+    } else if (BdComp > 2) {
         cerr << "wrong boundary part number: " << BdComp << endl;
     }
 }
