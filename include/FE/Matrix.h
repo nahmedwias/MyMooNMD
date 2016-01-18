4a28e789993b5694fcd56d5037b7648e5f3c7e69
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2016-01-18 12:10:10 +0100

    implemented methods TMatrix::multiply_with_transpose_from_right
    in fact Clemens did this in (WIAS-) MooNMD and I only copied it from there
    and updated the matrix test accordingly

--- include/FE/Matrix.h
+++ include/FE/Matrix.h
@@ -299,6 +299,44 @@ class TMatrix
      */ 
     TMatrix* multiply(const TMatrix * const B, double a = 1.0) const;
     
+    
+    /**
+     * @brief multiply this matrix B with its transposed B^T from the right
+     * 
+     * @return A pointer to the product matrix B*B^T.
+     */
+    TMatrix* multiply_with_transpose_from_right() const;
+
+    /** @brief multiply this matrix B with its transposed B^T from the right 
+     * and scale with a diagonal matrix in between. 
+     * 
+     * Compute the product B*D*B^T where B is this matrix and D is a diagonal 
+     * matrix.
+     *
+     * @param[in] diagonalScaling The diagonal scaling matrix D as a vector. 
+     * Must have as many entries as B has columns.
+     * @return A pointer to the product matrix B*D*B^T.
+     */
+    TMatrix* multiply_with_transpose_from_right(
+      const std::vector<double>& diagonalScaling) const;
+
+    /** @brief multiply this matrix B with its transposed B^T from the right 
+     * and scale with a diagonal matrix in between.
+     *
+     * Computing the structure of the product is computationally most 
+     * intensive. If it is known from similiar former computations, pass it as 
+     * an argument.
+     *
+     * @param[in] structure The known structure.
+     * @param[in] diagonalScaling The diagonal scaling matrix D as a vector. 
+     * Must have as many entries as B has columns.
+     * @return A pointer to the product matrix B*D*B^T.
+     */
+    TMatrix* multiply_with_transpose_from_right(
+      const std::vector<double>& diagonalScaling, const TStructure& structure) 
+    const;
+    
+    
     /** @brief adding a scaled matrix to this matrix
      * 
      * The summation is index-wise, i.e. A(i,j) += factor*m(i.j), where A is 
