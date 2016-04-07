9d2e7da4820db1ebab256f336f0ee8f66e13e009
Clemens Bartsch <bartsch@wias-berlin.de>
2016-04-07 16:25:31 +0200

    Small output adjustment, fixed a valgrind error.

--- src/FE/FE3D.C
+++ src/FE/FE3D.C
@@ -64,11 +64,8 @@ void TFE3D::CheckNFandBF()
 
   if(rank==TDatabase::ParamDB->Par_P0)
 #endif
-  if(TDatabase::ParamDB->SC_VERBOSE > 0)
-  {
-   cout << "CheckNFandBF: " << "BaseFunct_ID: " << BaseFunct_ID << " ";
-   cout << "NodalFunctional_ID: " << NodalFunctional_ID << endl;
-  }
+    Output::print<3>("CheckNFandBF: BaseFunct_ID: ", this->BaseFunct_ID,
+                  " NodalFunctional_ID: ", this->NodalFunctional_ID);
   for(k=0;k<N_DOF;k++)
   {
     for(l=0;l<N_Points;l++)
@@ -91,17 +88,11 @@ void TFE3D::CheckNFandBF()
 	  if(TDatabase::ParamDB->SC_VERBOSE > 0){
       if( i == k )
         if( fabs(FunctionalValues[i]-1) > 1e-8 )
-          cout << "BF: " << k << " NF: " << i << " " << FunctionalValues[i] << endl;
+          Output::print<3>("BF: ", k, " NF: ", i, " ", FunctionalValues[i]);
       if( i != k )
         if( fabs(FunctionalValues[i]-0) > 1e-8 )
-          cout << "BF: " << k << " NF: " << i << " " << FunctionalValues[i] << endl;
+          Output::print<3>("BF: ", k, " NF: ", i, " ", FunctionalValues[i]);
 	  }
     }
   }
-
-#ifdef _MPI
-  if(rank==TDatabase::ParamDB->Par_P0)
-#endif 
-  if(TDatabase::ParamDB->SC_VERBOSE > 0)
-  cout << endl;
 }
