4223930aadde2cdd4348a456e13bfaab2d520593
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2016-04-25 17:14:07 +0200

    included another constructor taking a TMatrix
    also the method solve not has a public overload which takes raw pointers
    (double*). This is not nice and might be reverted one day.

--- src/Solver/DirectSolver.C
+++ src/Solver/DirectSolver.C
@@ -222,6 +222,15 @@ DirectSolver::DirectSolver(const BlockFEMatrix& matrix,
 }
 
 /** ************************************************************************ */
+DirectSolver::DirectSolver(const TMatrix matrix,
+                           DirectSolver::DirectSolverTypes type)
+ : DirectSolver(std::make_shared<TMatrix>(matrix), type)
+{
+
+}
+
+
+/** ************************************************************************ */
 DirectSolver::DirectSolver(DirectSolver&& other)
  : type(other.type), matrix(other.matrix),
    cols(std::move(other.cols)), rows(std::move(other.rows)),
@@ -430,7 +439,6 @@ void DirectSolver::solve(const double* rhs, double* solution)
       double * entries = this->matrix->GetEntries();
       int * rows = this->matrix->GetRowPtr();
       int * cols = this->matrix->GetKCol();
-      double dzero; // ??
       int error;
       pardiso_(this->pt, &this->maxfct, &this->mnum, &this->mtype, &phase,
                &n_eq, entries, rows, cols, &this->perm, &this->nrhs,
