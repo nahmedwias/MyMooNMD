50a38002bcbb6b613ed5485ba3fab8ff0e056be7
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2016-06-08 14:55:50 +0200

    updated the implementation of an cd2d example on the unit square
    now you can change the exact solution and the boundary conditions (almost)
    arbitrarily. This is not a fast implementation but much more flexible.

--- include/Examples/NSE_2D/SinCos.h
+++ include/Examples/NSE_2D/SinCos.h
@@ -13,18 +13,18 @@ void ExampleFile()
 // ========================================================================
 void ExactU1(double x, double y, double *values)
 {
-    values[0] = sin(Pi*x);
-    values[1] = Pi*cos(Pi*x);
-    values[2] = 0;
-    values[3] = -Pi*Pi*sin(Pi*x);
+  values[0] = sin(Pi*x);
+  values[1] = Pi*cos(Pi*x);
+  values[2] = 0;
+  values[3] = -Pi*Pi*sin(Pi*x);
 }
 
 void ExactU2(double x, double y, double *values)
 {
-    values[0] = -Pi*y*cos(Pi*x);
-    values[1] = Pi*Pi*y*sin(Pi*x);
-    values[2] = -Pi*cos(Pi*x);
-    values[3] = Pi*Pi*Pi*y*cos(Pi*x);
+  values[0] = -Pi*y*cos(Pi*x);
+  values[1] = Pi*Pi*y*sin(Pi*x);
+  values[2] = -Pi*cos(Pi*x);
+  values[3] = Pi*Pi*Pi*y*cos(Pi*x);
 }
 
 void ExactP(double x, double y, double *values)
@@ -59,43 +59,71 @@ void BoundCondition(int i, double t, BoundCond &cond)
   TDatabase::ParamDB->INTERNAL_PROJECT_PRESSURE = 1;
 }
 
+void transform(const int BdComp, const double Param, double& x, double& y, 
+               double& nx, double& ny)
+{
+  switch(BdComp)
+  {
+    case 0:
+      x = Param;
+      y = 0.;
+      nx = 0.;
+      ny = -1.;
+      break;
+    case 1:
+      x = 1.;
+      y = Param;
+      nx = 1.;
+      ny = 0.;
+      break;
+    case 2:
+      x = 1. - Param;
+      y = 1.;
+      nx = 0.;
+      ny = 1.;
+      break;
+    case 3:
+      x = 0.;
+      y = 1. - Param;
+      nx = -1.;
+      ny = 0.;
+      break;
+    default:
+      ErrThrow("wrong boundary part number", BdComp);
+      break;
+  }
+}
+
 void U1BoundValue(int BdComp, double Param, double &value)
 {
   // Neumann boundary means setting T.n where n is outer normal and T is either
   // 2nu D(u) - p  (for LAPLACETYPE==1) or nu grad(u) - p   (for LAPLACETYPE==0)
   const int lt = TDatabase::ParamDB->LAPLACETYPE;
   const double nu = 1./TDatabase::ParamDB->RE_NR;
+  // find out boundary condition at the evaluation point on the boundary
   BoundCond cond;
   BoundCondition(BdComp, Param, cond);
-  switch(BdComp)
+  // find coordinates and normal of evaluation point on the boundary
+  double x, y, nx, ny;
+  transform(BdComp, Param, x, y, nx, ny);
+  // evaluate the exact solution at the given point
+  double u1[4];
+  double u2[4];
+  double p[4];
+  ExactU1(x, y, u1);
+  ExactU2(x, y, u2);
+  ExactP(x, y, p);
+  if(cond == DIRICHLET)
+    value = u1[0];
+  else
   {
-    case 0:
-      if(cond == DIRICHLET)
-        value = sin(Pi*Param); // Dirichlet
-      else
-        value = lt == 0 ? 0. : -nu*Pi*Pi*sin(Pi*Param); // Neumann
-      break;
-    case 1:
-      if(cond == DIRICHLET)
-        value = 0.; // Dirichlet
-      else
-        value = lt == 0 ? -nu*Pi : -2*nu*Pi; // Neumann
-      break;
-    case 2:
-      if(cond == DIRICHLET)
-        value = sin(Pi*(1-Param)); // Dirichlet
-      else
-        value = lt == 0 ? 0. : nu*Pi*Pi*sin(Pi*(1-Param)); // Neumann
-      break;
-    case 3:
-      if(cond == DIRICHLET)
-        value = 0.; // Dirichlet
-      else
-        value = lt == 0 ? -nu*Pi : -2*nu*Pi; // Neumann
-      break;
-    default:
-      ErrThrow("wrong boundary part number", BdComp);
-      break;
+    // NEUMANN
+    value = nu * (nx * u1[1] + ny * u1[2]) - p[0] * nx;
+    if(lt == 1)
+    {
+      value *= 0.5;
+      value += 0.5 * nu * (nx * u1[1] + ny * u2[1]);
+    }
   }
   return;
 }
@@ -106,43 +134,30 @@ void U2BoundValue(int BdComp, double Param, double &value)
   // 2nu D(u) - p  (for LAPLACETYPE==1) or nu grad(u) - p   (for LAPLACETYPE==0)
   const int lt = TDatabase::ParamDB->LAPLACETYPE;
   const double nu = 1./TDatabase::ParamDB->RE_NR;
+  // find out boundary condition at the evaluation point on the boundary
   BoundCond cond;
   BoundCondition(BdComp, Param, cond);
-  switch(BdComp)
+  // find coordinates and normal of evaluation point on the boundary
+  double x, y, nx, ny;
+  transform(BdComp, Param, x, y, nx, ny);
+  // evaluate the exact solution at the given point
+  double u1[4];
+  double u2[4];
+  double p[4];
+  ExactU1(x, y, u1);
+  ExactU2(x, y, u2);
+  ExactP(x, y, p);
+  if(cond == DIRICHLET)
+    value = u2[0];
+  else
   {
-    case 0:
-      if(cond == DIRICHLET)
-        value = 0; // Dirichlet
-      else
-      {
-        value = nu*Pi*cos(Pi*Param) * (lt == 0 ? 1. : 2.); // Neumann
-        value += sin(Pi*Param);// Neumann (from pressure)
-      }
-      break;
-    case 1:
-      if(cond == DIRICHLET)
-        value = Pi*Param; // Dirichlet
-      else
-        value = 0.; // Neumann
-      break;
-    case 2:
-      if(cond == DIRICHLET)
-        value = -Pi*cos(Pi*(1-Param)); // Dirichlet
-      else
-      {
-        value = -nu*Pi*cos(Pi*(1-Param)) * (lt == 0 ? 1. : 2.); // Neumann
-        value += sin(Pi*(1-Param));// Neumann (from pressure)
-      }
-      break;
-    case 3:
-      if(cond == DIRICHLET)
-        value = -Pi*(1-Param); // Dirichlet
-      else
-        value = 0.; // Neumann
-      break;
-    default:
-      ErrThrow("wrong boundary part number", BdComp);
-      break;
+    // NEUMANN
+    value = nu * (nx * u2[1] + ny * u2[2]) - p[0] * ny;
+    if(lt == 1)
+    {
+      value *= 0.5;
+      value += 0.5 * nu * (nx * u1[2] + ny * u2[2]);
+    }
   }
   return;
 }
