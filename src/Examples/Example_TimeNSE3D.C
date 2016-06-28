b42cdfd12d14277f75643d17cbaa6e0fec19aa19
Najib Alia <Najib Alia@localhost>
2016-06-28 16:31:22 +0200

    Added Flow_around_cylinder for TNSE3D

--- src/Examples/Example_TimeNSE3D.C
+++ src/Examples/Example_TimeNSE3D.C
@@ -1,4 +1,5 @@
 #include <Example_TimeNSE3D.h>
+#include <Time_NSE3D.h>
 #include <FEDatabase3D.h>
 #include <Database.h>
 #include <MainUtilities.h>
@@ -27,11 +28,15 @@ namespace Bsp3
 {
   #include "TNSE_3D/Bsp3.h"   // 5
 }
-
 namespace tunnel_tdp1
 {
-#include "TNSE_3D/Tunnel1.h"
+#include "TNSE_3D/Tunnel1.h"  // 6
 }
+namespace flow_around_cylinder_instationary
+{
+#include "TNSE_3D/FlowAroundCylinder_instat.h"   // 7
+}
+
 //=========================================================
 Example_TimeNSE3D::Example_TimeNSE3D(int example_code,
                                      const ParameterDatabase& user_input_parameter_db)
@@ -263,6 +268,42 @@ Example_TimeNSE3D::Example_TimeNSE3D(int example_code,
       ExampleFile();
       break;
     }
+    case 7:
+    {
+      using namespace flow_around_cylinder_instationary;
+      /** exact_solution */
+      exact_solution.push_back( ExactU1 );
+      exact_solution.push_back( ExactU2 );
+      exact_solution.push_back( ExactU3 );
+      exact_solution.push_back( ExactP );
+
+      /** boundary condition */
+      boundary_conditions.push_back( BoundCondition );
+      boundary_conditions.push_back( BoundCondition );
+      boundary_conditions.push_back( BoundCondition );
+      boundary_conditions.push_back( BoundConditionNoBoundCondition );
+
+      /** boundary values */
+      boundary_data.push_back( U1BoundValue );
+      boundary_data.push_back( U2BoundValue );
+      boundary_data.push_back( U3BoundValue );
+      boundary_data.push_back( BoundaryValueHomogenous );
+
+      /** coefficients */
+      problem_coefficients = LinCoeffs;
+
+      /** initial conditions */
+      initialCondtion.push_back( InitialU1 );
+      initialCondtion.push_back( InitialU2 );
+      initialCondtion.push_back( InitialU3 );
+
+      /**post processing - drag and lift calculation and output */
+      post_processing_stat = compute_drag_lift_pdiff;
+
+      nu = flow_around_cylinder_instationary::get_nu();
+      ExampleFile();
+      break;
+    }
     default:
       ErrThrow("Unknown Example_TimeNSE3D example!");
   }
@@ -270,7 +311,19 @@ Example_TimeNSE3D::Example_TimeNSE3D(int example_code,
 
 void Example_TimeNSE3D::do_post_processing(Time_NSE3D& tnse3d) const
 {
-  //TODO
+  if(post_processing_stat)
+  {
+    post_processing_stat(tnse3d);
+  }
+  else
+  {
+#ifdef _MPI
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    if (my_rank == 0)
+#endif
+      Output::info<2>("Example_TimeNSE3D","No post processing done for the current example.");
+  }
 }
 
 double Example_TimeNSE3D::get_nu() const
