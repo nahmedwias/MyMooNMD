ca054e7dd52e6df75000b0ee4ef75d0e914f2eb8
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2016-07-14 12:58:37 +0200

    started a class PETScSolver as a wrapper for PETSc as a solver
    this is the first try. It does not work for saddle point problems yet. Also
    none of out usual solver parameters are used. Some fixed preconditioner is
    applied.
    
    Currently only the sequential version is working.
    
    Again, this is the start and needs further development.

---
+++ include/Solver/PETScSolver.h
@@ -0,0 +1,54 @@
+/** ************************************************************************ 
+*
+* @class PETScSolver
+* @brief solving a linear system using PETSc
+****************************************************************************/
+
+#ifndef __PETSCSOLVER__
+#define __PETSCSOLVER__
+
+#include <petscksp.h> // defines type 'Mat' below and much more
+
+// forward declaration
+class BlockMatrix;
+class BlockFEMatrix;
+class BlockVector;
+
+class PETScSolver
+{
+  public:
+    /** @brief constructor: an internal copy is created and stored. Ready to 
+     * solve.
+     */
+    PETScSolver(const BlockFEMatrix& matrix);
+    PETScSolver(const BlockMatrix& matrix);
+    
+    /** @brief This class is not copy constructible */
+    PETScSolver(const PETScSolver&) = delete;
+
+    /** @brief move constructor */
+    PETScSolver(PETScSolver&&);
+
+    /** @brief This class is not copy assignable */
+    PETScSolver& operator=(const PETScSolver&) = delete;
+
+    /** @brief move assignment operator */
+    PETScSolver& operator=(PETScSolver&&);
+    
+    /** @brief release all memory */
+    ~PETScSolver();
+
+    /**
+     * @brief Solves the equation A*(solution)=rhs for solution.
+     * 
+     * @param rhs the right-hand side of the problem Ax=b
+     * @param solution vector to store the solution into
+     */
+    void solve(const BlockVector& rhs, BlockVector& solution);
+    
+  private:
+    /** @brief the matrix of the linear equation A*x=b, PETSc format */
+    Mat petsc_mat;
+};
+#endif // __PETSCSOLVER__
+
