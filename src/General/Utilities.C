c833331551a7dde26c95d119cae7619ffd69385f
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2016-07-17 22:14:25 +0200

    removed old weird methods in Utilities file. Added a namespace utilities
    this is where we can collect useful method which do not really belong into a
    class. Updated the old WriteParamDB method accordingly

--- src/General/Utilities.C
+++ src/General/Utilities.C
@@ -1,64 +1,19 @@
-/** rearrange bytes in int array */
-void SwapIntArray(int *intarray, int length)
+#include <Utilities.h>
+#include <chrono>
+#include <ctime>
+#include <unistd.h>
+
+std::string utilities::get_host_name()
 {
-  char *array, c;
-  int i;
+  char buf[80];
+  gethostname(buf,80);
+  return std::string(buf);
+}
 
-  for(i=0;i<length;i++)
-  {
-    array = (char *)(intarray+i);
-    c = array[0];
-    array[0] = array[3];
-    array[3] = c;
 
-    c = array[1];
-    array[1] = array[2];
-    array[2] = c;
-  }
-} // SwapIntArray
-
-/** rearrange bytes in float array */
-void SwapFloatArray(float *floatarray, int length)
+std::string utilities::get_date_and_time()
 {
-  char *array, c;
-  int i;
-
-  for(i=0;i<length;i++)
-  {
-    array = (char *)(floatarray+i);
-    c = array[0];
-    array[0] = array[3];
-    array[3] = c;
-
-    c = array[1];
-    array[1] = array[2];
-    array[2] = c;
-  }
-} // SwapFloatArray
-
-/** rearrange bytes in double array */
-void SwapDoubleArray(double *doublearray, int length)
-{
-  char *array, c;
-  int i;
-
-  for(i=0;i<length;i++)
-  {
-    array = (char *)(doublearray+i);
-    c = array[0];
-    array[0] = array[7];
-    array[7] = c;
-
-    c = array[1];
-    array[1] = array[6];
-    array[6] = c;
-
-    c = array[2];
-    array[2] = array[5];
-    array[5] = c;
-
-    c = array[3];
-    array[3] = array[4];
-    array[4] = c;
-  }
-} // SwapDoubleArray
+  auto time = std::chrono::system_clock::now();
+  std::time_t t = std::chrono::system_clock::to_time_t(time);
+  return std::ctime(&t);
+}
