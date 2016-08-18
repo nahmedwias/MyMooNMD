98850def2ec7cf268a6526f37705b66a35e4dcf8
caiazzo <caiazzo@localhost>
2016-08-18 12:34:48 +0200

    new class for assembling updated (ongoing)

--- include/FE/Assembler3.h
+++ include/FE/Assembler3.h
@@ -10,19 +10,22 @@
  *
  ******************************************************************************/
 
-#ifndef __ASSEMBLER2D__
-#define __ASSEMBLER2D__
+#pragma once
 
 #include <Constants.h>
 #include <FEDatabase2D.h>
 #include <LocalAssembling2D.h>
+#include <BlockVector.h>
+#include <BlockFEMatrix.h>
 #include <Example2D.h>
 
 #ifdef __3D__
   #include <Aux2D3D.h>
 #endif
 
-
+/**
+   @brief Todo...
+ */
 
 class Assembler3{
     
@@ -30,58 +33,77 @@ protected:
     
 public:
     
-    /** @brief constructor
+    /** @brief constructor (taken from LocalAssembling)
      *
-     * This constructor calls the other constructor creating an ??
-     * object for you. See there for more documentation.
+     * In the current version, the Assembler object is created using
+     * a constructor analogous to LocalAssembling.
+     * This is just a temporary solution and should probably done differently
      */
-    Assembler3(LocalAssembling2D_type type, TFEFunction2D **fefunctions2d,
-                                    CoeffFct2D *coeffs);
-    
-//    LocalAssembling2D la(LocalAssembling2D_type type, TFEFunction2D **fefunctions2d,
-//                         CoeffFct2D *coeffs);
-    
-    
-    LocalAssembling2D la;
-    
+  Assembler3(LocalAssembling2D_type type, TFEFunction2D **fefunctions2d,
+	     CoeffFct2D *coeffs);
+  
+  ///@brief The variational form to be assembled
+  LocalAssembling2D la;
+  ///@brief The collection on which we want to assemble
+  TCollection *Coll;
+  
+  ///@brief Vector for handling hanging nodes
+  std::vector< std::vector<double> > hangingEntries, hangingRhs;
+  
+  ///@brief blocks to be assembled
+  std::vector< TSquareMatrix2D* > square_matrices;
+  std::vector< TMatrix2D* > rectangular_matrices;
+  std::vector< double* > rhs_blocks;
+  
+  /** a function from a finite element space */
+  void Assemble2D(BlockFEMatrix &M,BlockVector &b_rhs,
+		  std::vector <const TFESpace2D*>& fespaces,
+		  std::vector <const TFESpace2D*>& ferhs,
+		  const Example2D& example,
+		  int AssemblePhaseID=-1
+		  );
+  
+  /**
+     @brief Set the variables of the class (hanging nodes, allocate pointers)
+     
+     This is a temporary solution.
+  */
+  void init(BlockFEMatrix &M,BlockVector &b_rhs,
+	    std::vector<const TFESpace2D*>& fespaces,
+	    std::vector<const TFESpace2D*>& ferhs);
 
-    
-    /** a function from a finite element space */
-    void Assemble2D(int n_fespaces, const TFESpace2D **fespaces,
-                    int n_sqmatrices, TSquareMatrix2D **sqmatrices,
-                    int n_matrices, TMatrix2D **matrices,
-                    int n_rhs, double **rhs, const TFESpace2D **ferhs,
-                    //BoundCondFunct2D **BoundaryConditions,
-                    //BoundValueFunct2D **BoundaryValues,
-                    //LocalAssembling2D& la,
-                    const Example2D& example,
-                    int AssemblePhaseID=-1
-                    );
-    
-    
-    
-    void loop_over_cells(const TFESpace2D** fespaces, int AssemblePhaseID, int n_fespaces,
-                         //LocalAssembling2D& la,
-                         int n_sqmatrices, double ***LocMatrices,
-                         TSquareMatrix2D **sqmatrices,int n_matrices,TMatrix2D **matrices,
-                         int **TestGlobalNumbers, int **TestBeginIndex,
-                         int **AnsatzGlobalNumbers, int **AnsatzBeginIndex, double **HangingEntries, double **HangingRhs,
-                         double *righthand, int **RhsBeginIndex, int **RhsGlobalNumbers,
-                         //BoundCondFunct2D** BoundaryConditions,
-                         //BoundValueFunct2D** BoundaryValues,
-                         int N_AllMatrices, double **Matrices, double **Param, int n_rhs, double** rhs,
-                         double **AuxArray,double **LocRhs,const TFESpace2D** ferhs,
-                         int *N_BaseFunct,  BaseFunct2D *BaseFuncts,int **GlobalNumbers, int **BeginIndex,
-                         const Example2D& example);
-    
-    
-    ~Assembler3();
-    
-   
+  /**
+     @brief loop over all cells and assemble local terms
+   */
+  void loop_over_cells(std::vector <const TFESpace2D*>& fespaces,
+		       int AssemblePhaseID,
+		       double ***LocMatrices,
+		       double *righthand,
+		       int N_AllMatrices, double **Matrices,
+		       double **Param,
+		       double **AuxArray,double **LocRhs,
+		       std::vector<const TFESpace2D*>& ferhs,
+		       const Example2D& example);
+  
+  void imposeBoundaryConditions(const TFESpace2D *fespace, int j,
+				const Example2D& example,
+				TBaseCell* cell,
+				int i,
+				double *RHS,
+				int *DOF);
 
+  
+  void handleHangingNodes(std::vector<const TFESpace2D*>& ferhs);
 
-    
-    };
-#endif // __ASSEMBLE2D__
-//double *Param[MaxN_QuadPoints_2D]
-//double *AuxArray[MaxN_QuadPoints_2D]
+ 
+  void addLocalToGlobalRhs(TBaseCell *cell, int i,
+			   std::vector<const TFESpace2D*>& ferhs,
+			   double *righthand);
+  
+  ~Assembler3();
+  
+  
+  
+  
+  
+};
