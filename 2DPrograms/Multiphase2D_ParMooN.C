19f4d6ddbe8518dacd3aad06567518b7af2cf92d
Najib Alia <Najib Alia@localhost>
2016-09-13 14:32:41 +0200

    Creating new branch for multiphase flow

---
+++ 2DPrograms/Multiphase2D_ParMooN.C
@@ -0,0 +1,27 @@
+// =======================================================================
+//
+// Purpose:     main program for solving a stationary NSE equation in ParMooN
+//
+// Author:      Sashikumaar Ganesan
+//
+// History:     Implementation started on 23.08.2014
+
+// =======================================================================
+#include <Domain.h>
+#include <Database.h>
+#include <FEDatabase2D.h>
+#include <NSE2D.h>
+#include <Example_NSE2D.h>
+#include <Chrono.h>
+#include <LoopInfo.h>
+#include <ParameterDatabase.h>
+
+
+// =======================================================================
+// main program
+// =======================================================================
+int main(int argc, char* argv[])
+{
+  cout << "CECI EST MON PREMIER PROGRAMME." << endl;
+  return 0;
+} // end main
