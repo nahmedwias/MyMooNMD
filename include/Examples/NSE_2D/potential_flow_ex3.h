9ee641e087fe48297832511b7279f6d7da8aa84b
ahmed <ahmed@wias-berlin.de>
2016-10-04 18:05:46 +0200

    new example is included and main program is updated

---
+++ include/Examples/NSE_2D/potential_flow_ex3.h
@@ -0,0 +1,102 @@
+#ifndef POTENTIAL_FLOW_EX3_H
+#define POTENTIAL_FLOW_EX3_H
+
+#include <math.h>
+
+double DIMENSIONLESS_VISCOSITY;
+void ExampleFile()
+{
+  Output::print("Example: NSE_2D/potential_flow_ex3.h");
+  // TDatabase::ParamDB->INTERNAL_QUAD_RULE=99;
+  Output::print("Internal quadrature rule ",
+                TDatabase::ParamDB->INTERNAL_QUAD_RULE);
+}
+// ========================================================================
+// exact solution
+// ========================================================================
+void ExactU1(double x, double y, double *values)
+{
+    values[0] = 0; values[1] = 0; values[2] = 0;
+}
+
+void ExactU2(double x, double y, double *values)
+{
+  values[0] = 0;values[1] = 0; values[2] = 0;
+}
+
+void ExactP(double x, double y, double *values)
+{
+  values[0] = (-157*sqrt(0.00017886802095311101))/12. +
+              2*sqrt(0.04024530471444998)*pow(x,3)*y +
+              2*sqrt(0.04024530471444998)*pow(x,2)*pow(y,2) +
+              2*sqrt(0.04024530471444998)*pow(y,4);
+
+  values[1] = 6*sqrt(0.04024530471444998)*pow(x,2)*y +
+              4*sqrt(0.04024530471444998)*x*pow(y,2);
+
+  values[2] = 2*sqrt(0.04024530471444998)*pow(x,3) +
+              4*sqrt(0.04024530471444998)*pow(x,2)*y +
+              8*sqrt(0.04024530471444998)*pow(y,3);
+}
+
+// ========================================================================
+// boundary conditions
+// ========================================================================
+void BoundCondition(int i, double t, BoundCond &cond)
+{
+  cond = DIRICHLET;
+  TDatabase::ParamDB->INTERNAL_PROJECT_PRESSURE = 1;
+}
+
+
+void U1BoundValue(int BdComp, double Param, double &value)
+{
+//  double x=0, y=0;
+
+//  switch(BdComp)
+//  {
+//    case 0:  x = Param; y=0;      break;
+//    case 1:  x = 1; y = Param;    break;
+//    case 2:  x = 1-Param; y = 1;  break;
+//    case 3:  x = 0; y = 1-Param;  break;
+//    default: cout << "wrong boundary part number" << endl;
+//      break;
+//  }
+  value=0;
+}
+
+void U2BoundValue(int BdComp, double Param, double &value)
+{
+//  double x=0, y=0;
+
+//  switch(BdComp)
+//  {
+//    case 0:  x = Param; y=0;     break;
+//    case 1:  x = 1; y = Param;   break;
+//    case 2:  x = 1-Param; y = 1; break;
+//    case 3:  x = 0; y = 1-Param; break;
+//    default: cout << "wrong boundary part number" << endl;
+//      break;
+//  }
+ value=0;
+}
+
+// ========================================================================
+// coefficients for Stokes form: A, B1, B2, f1, f2
+// ========================================================================
+void LinCoeffs(int n_points, double *X, double *Y,
+               double **parameters, double **coeffs)
+{
+  static double nu = DIMENSIONLESS_VISCOSITY;
+  double valp[4];
+
+  for(int i=0;i<n_points;i++)
+  {
+    ExactP(X[i], Y[i], valp);
+    coeffs[i][0] = nu;
+    coeffs[i][1] = valp[1];
+    coeffs[i][2] = valp[2];
+  }
+}
+
+#endif // POTENTIAL_FLOW_EX3_H
--- src/Examples/Example_TimeNSE2D.C
+++ src/Examples/Example_TimeNSE2D.C
@@ -27,6 +27,10 @@ namespace potential_flow_ex2
 #include "TNSE_2D/potential_flow_td_ex2.h"
 }
 
+namespace potential_flow_ex3 {
+#include "TNSE_2D/potential_flow_ex3.h"
+}
+
 Example_TimeNSE2D::Example_TimeNSE2D(
   const ParameterDatabase& user_input_parameter_db)
  : Example_NonStationary2D(user_input_parameter_db)
@@ -163,6 +167,32 @@ Example_TimeNSE2D::Example_TimeNSE2D(
       potential_flow_ex2::DIMENSIONLESS_VISCOSITY=this->get_nu();
       potential_flow_ex2::ExampleFile();
       break;
+  case 5:
+        /** exact_solution */
+        exact_solution.push_back( potential_flow_ex3::ExactU1 );
+        exact_solution.push_back( potential_flow_ex3::ExactU2 );
+        exact_solution.push_back( potential_flow_ex3::ExactP );
+
+        /** boundary condition */
+        boundary_conditions.push_back( potential_flow_ex3::BoundCondition );
+        boundary_conditions.push_back( potential_flow_ex3::BoundCondition );
+        boundary_conditions.push_back( BoundConditionNoBoundCondition );
+
+        /** boundary values */
+        boundary_data.push_back( potential_flow_ex3::U1BoundValue );
+        boundary_data.push_back( potential_flow_ex3::U2BoundValue );
+        boundary_data.push_back( BoundaryValueHomogenous );
+
+        /** initial conditions, in case of a non-stationary problem */
+        initialCOndtion.push_back(potential_flow_ex3::InitialU1);
+        initialCOndtion.push_back(potential_flow_ex3::InitialU2);
+        initialCOndtion.push_back(potential_flow_ex3::InitialP);
+        /** coefficients */
+        problem_coefficients = potential_flow_ex3::LinCoeffs;
+
+        potential_flow_ex3::DIMENSIONLESS_VISCOSITY=this->get_nu();
+        potential_flow_ex3::ExampleFile();
+        break;
     default:
       ErrThrow("Unknown Time dependent Example_TimeNSE2D example!");
   }
