5c54e1de718d1c38d41a1066d48328d770950ef4
Najib Alia <Najib Alia@localhost>
2016-10-07 14:50:25 +0200

    TEST FAILED.
    No effect of 2 way coupling with 2 different fluids on the poiseuille flow observed...The calculation of the new property fields with the new phase fraction is calculated correctly (tested by putting phase fraction = 0.5 everywhere and results = correct= average of the 2 fluids'properties'fields).
    
    Further investigation needed...

--- user_projects/Multiphase2D_ParMooN.C
+++ user_projects/Multiphase2D_ParMooN.C
@@ -59,7 +59,7 @@ int main(int argc, char* argv[])
   nse_db.merge(parmoon_db,true);
   cd_db.merge(parmoon_db,true);
 
-  cd_db["example"]          = 10;    // 10 = Example Layout for CD2d which takes input velocity
+  cd_db["example"]          = 11;    // 10 = Example Layout for CD2d which takes input velocity
   cd_db["problem_type"]     = 1;
   cd_db["output_basename"]  = "multiphase_convection_output";
 
@@ -95,9 +95,9 @@ int main(int argc, char* argv[])
   CD2D          cd2d(domain, cd_db);                    // Construct CD system
 
   double rho1 = nse_db["fluid_density"];  // density constant of fluid1, eg 1000
-  double rho2 = 1000;                        // density constant of fluid2, eg 0
+  double rho2 = 700;                        // density constant of fluid2, eg 0
   double mu1  = nse_db["fluid_dynamic_viscosity"];    // mu constant of fluid1, eg 1e-3
-  double mu2  = 100;                                    // mu constant of fluid2, eg 0
+  double mu2  = 7;                                    // mu constant of fluid2, eg 0
 
 
 
