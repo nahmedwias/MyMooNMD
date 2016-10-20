3cc8bf3731b0cec96a42208a1011711021ce4ddf
Najib Alia <Najib Alia@localhost>
2016-10-20 10:54:35 +0200

    Tests OK.
    As expected, no changes.
    
    Next step: now there is some a little serious work to do. Choose one tcd2d example. I saw that they all have very easy convective fields like constants vectors (1,2) or something like that. Create a TNSE2D example to generate such a field. Test the TNSE2D example separately and check its paraview to see that the generated vectors are ok (deactivate coupling_nse_cd ==> false). Then, change TCD2D example to read parameters[] instead of b1 and b2. And Check.

--- user_projects/TimeMultiphase2D_ParMooN.C
+++ user_projects/TimeMultiphase2D_ParMooN.C
@@ -60,7 +60,7 @@ int main(int argc, char* argv[])
   tnse_db.merge(parmoon_db,true);
   tcd_db.merge(parmoon_db,true);
 
-  tcd_db["example"]          = 13;
+  tcd_db["example"]          = 12;
   tcd_db["problem_type"]     = 2;
   tcd_db["output_basename"]  = "multiphase_tconvection_output";
 
@@ -210,8 +210,8 @@ int main(int argc, char* argv[])
       {
         Output::print<1>("<<<<<<<<<<<<<<<<<< NOW SOLVING CONVECTION  >>>>>>>>>>>>>");
         Output::print<1>("================== JE COMMENCE A ASSEMBLER =============");
-        tcd2d.assemble_stiffness_matrix_alone();   // this line is outcommented when you want to make hand tests
-//        tcd2d.assemble_stiffness_matrix_alone_with_convection(&tnse2d.get_velocity());
+//        tcd2d.assemble_stiffness_matrix_alone();   // this line is outcommented when you want to make hand tests
+        tcd2d.assemble_stiffness_matrix_alone_with_convection(&tnse2d.get_velocity());
         tcd2d.scale_stiffness_matrix();
         Output::print<1>("================== JE COMMENCE A RESOUDRE  =============");
         tcd2d.solve();
