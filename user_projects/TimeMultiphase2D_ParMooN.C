c2e1c91b8648b4110f5d0c285a4446b7f6ff8075
Najib Alia <Najib Alia@localhost>
2016-12-14 16:37:54 +0200

    Re-launched the bunch of validation tests.
    Now, the coupling nse>cd is clean after having managed the interpolation thing.
    There are still some minor differences in the tests 32 and 33, between coupling cd>nse activated and deactivated. Needs further investigation to be sure that the code is correct: do like it was done in the previous commits 1976 to 1978: display analytical values and Param values of the viscosity and check it.
    
    This is the next step. + make the interpolation correctly as it was done for Time_cd2d.
    
    Now another point that was tested in this commit but then erased for lisibility purpose: when we activate AFC, a parameter FULL_INTERNAL_MATRIX is put to 1, and it destroys the BlockFEMatrix of TNSE2D, even when TCD2D is not solved! this is because of global dependency...
    
    This issue is the main remaining issue to get AFC work correctly in the program.
    But first, the above step

--- user_projects/TimeMultiphase2D_ParMooN.C
+++ user_projects/TimeMultiphase2D_ParMooN.C
@@ -80,8 +80,8 @@ int main(int argc, char* argv[])
       tnse_db["coupling_cd_nse"] = false;
       break;
     case 20: case 21: case 22:
-//      tnse_db["dimensional_nse"] = true;
-//      tcd_db["coupling_nse_cd"]  = true;
+      tnse_db["dimensional_nse"] = true;
+      tcd_db["coupling_nse_cd"]  = true;
       tnse_db["coupling_cd_nse"] = false;
       break;
     case 30: case 31: case 32:
@@ -159,7 +159,7 @@ int main(int argc, char* argv[])
   TFEFunction2D rho_field = update_fieldfunction(&tcd2d.get_space(),rho_vector,(char*)"r");
   TFEFunction2D mu_field  = update_fieldfunction(&tcd2d.get_space(),mu_vector,(char*)"m");
 
-//  // Interpolate the fields
+//  // Interpolate the fields THIS MUST BE MOVED IN THE ASSEMBLE METHOD!
 //  const TFESpace2D* into_space = &tnse2d.get_velocity_space();
 //  FEFunctionInterpolator interpolator(into_space);
 //  size_t length_interpolated = rho_field0.GetLength();
@@ -189,7 +189,7 @@ int main(int argc, char* argv[])
     else
       tcd2d.assemble_initial_time();
   }
-  exit(0);
+//  exit(0);
    double end_time = TDatabase::TimeDB->ENDTIME;
    int step = 0;
    int n_substeps = GetN_SubSteps();
