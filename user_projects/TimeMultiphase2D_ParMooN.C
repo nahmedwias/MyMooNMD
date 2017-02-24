afa3cab4ce59d34510aac6c5190eea8650503e0f
Najib Alia <Najib Alia@localhost>
2017-02-24 20:16:04 +0200

    Added and tested small example where gas bubble is floating in liquid bath.
    Purpose is to test gas injection (boundary condition), but first, tested simple case where a gas circle is emprisoned in liquid bath. First images show it is climbing but too much smearing. The circle becomes very small. Probably due to absence of interfacial tension.
    
    Next step: change BC and test

--- user_projects/TimeMultiphase2D_ParMooN.C
+++ user_projects/TimeMultiphase2D_ParMooN.C
@@ -133,7 +133,7 @@ int main(int argc, char* argv[])
   // both fluid properties are needed only in Dam break so far
   double rho2 = 0;        // density constant of gas, eg 0
   double mu2  = 0;        // mu constant of gas, eg 0
-  if (tcd_example_number == 40)
+  if (tcd_example_number == 40 ||tcd_example_number == 50 )
   {
   double density_ratio = TDatabase::ParamDB->P1;
   double viscosity_ratio = TDatabase::ParamDB->P2;
@@ -179,7 +179,7 @@ int main(int argc, char* argv[])
         rho1=1;
         rho_vector = rho1;  // uncommented because rho must stay constant=1
         break;
-      case 40: // dam break, both rho and mu are variable, nothing happens
+      case 40: case 50: // dam break, both rho and mu are variable, nothing happens
         break;
       default:
         ErrThrow("Coupling CD>NSE is not allowed in example "
