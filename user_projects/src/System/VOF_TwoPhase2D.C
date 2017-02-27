76186ea9fe4c2bf69b76b4fb469a33ee4f43fcc9
Najib Alia <Najib Alia@localhost>
2017-02-27 17:50:13 +0200

    Finished implementation of VOF_TwoPhase2D class in the main program.
    Next step:launch all the tests to validate/correct possible bugs.

--- user_projects/src/System/VOF_TwoPhase2D.C
+++ user_projects/src/System/VOF_TwoPhase2D.C
@@ -1,6 +1,5 @@
 #include <VOF_TwoPhase2D.h>
 
-
 /** @brief constructor*/
 VOF_TwoPhase2D::VOF_TwoPhase2D(const TDomain& domain,
                                const ParameterDatabase& param_db_tnse,
@@ -10,25 +9,30 @@ VOF_TwoPhase2D::VOF_TwoPhase2D(const TDomain& domain,
   tnse2d_(domain,param_db_tnse,example_tnse2d_),
   example_tcd2d_(param_db_tcd),
   phaseconvection2d_(domain,param_db_tcd,example_tcd2d_),
+  /* Properties of liquid phase, eg. rhol=1000 and mul=1.e-3 */
+  rhol_(param_db_tnse["fluid_density"]),
+  mul_(param_db_tnse["fluid_dynamic_viscosity"]),
+  /* List of booleans */
   tnse_variable_fluid_(param_db_tnse["dimensional_nse"]),
   solve_convection_(param_db_tcd["solve_cd"]),
   nse2cd_coupling_(param_db_tcd["coupling_nse_cd"]),
   cd2nse_coupling_(param_db_tnse["coupling_cd_nse"]),
+  /* Copy the block vector structure */
   rho_vector_(phaseconvection2d_.get_solution()),
-  mu_vector_(phaseconvection2d_.get_solution())
+  mu_vector_(phaseconvection2d_.get_solution()),
+  unity_vector_(phaseconvection2d_.get_solution()),
+  /* Construct FeFunctions */
+  rho_fefunction_(&this->phaseconvection2d_.get_space(),(char*)"r",(char*)"r",
+                  rho_vector_.block(0),rho_vector_.length(0)),
+  mu_fefunction_(&this->phaseconvection2d_.get_space(),(char*)"m",(char*)"m",
+                  mu_vector_.block(0),mu_vector_.length(0))
 {
-  /* First, check and set consistent parameters  */
-  manage_example_parameters();
-
-  /* Properties of liquid phase, eg. rhol=1000 and mul=1.e-3 */
-  rhol_ = param_db_tnse["fluid_density"];
-  mul_  = param_db_tnse["fluid_dynamic_viscosity"];
-
-  /* Initialize vectors to constant values of one phase, e.g. liquid  */
-  this->rho_vector_ = rhol_;
-  this->mu_vector_ = mul_;
-
-
+  /* Initialize unity vector  */
+  this->unity_vector_ = 1;
+/* at the end of the constructor, rho and mu are constant equal to phase fraction,
+ * whatever the examples and booleans are. The call to "update_field_vectors" will
+ * calculate their value, depending on the example number
+ */
 }
 
 /** Check and set input parameters */
@@ -53,51 +57,74 @@ void VOF_TwoPhase2D::manage_example_parameters()
     case 10:
       this->nse2cd_coupling_  = false;
       this->cd2nse_coupling_  = false;
+      Output::info<5>("Example " + std::to_string(example_number_) +
+                      ":the couplings must be deactivated, but you are free "
+                      "to activate 'tnse_variable_properties' and 'solve tcd'.");
       break;
     case 20: case 21: case 22:
       this->tnse_variable_fluid_ = true;
       this->nse2cd_coupling_     = true;
       this->cd2nse_coupling_     = false;
+      this->solve_convection_    = true;
+      Output::info<5>("Example " + std::to_string(example_number_) +
+                      ":all booleans must be true but cd2nse_coupling is false.");
       break;
     case 30: case 31: case 32:
 //      this->tnse_variable_fluid_ = true;
       this->nse2cd_coupling_  = false;
 //      this->cd2nse_coupling_  = true;
 //      this->solve_convection_ = true;
+      Output::info<5>("Example " + std::to_string(example_number_) +
+                      ":coupling nse2cd must be false, but the other booleans"
+                      " are free to be changed.");
       break;
-    case 40: case 41:
+    case 40: case 41: case 50:
 //      this->tnse_variable_fluid_ = true;
 //      this->nse2cd_coupling_  = true;
 //      this->cd2nse_coupling_  = true;
 //      this->solve_convection_ = true;
       break;
   }
-
-
-  switch(example_number_)
-  {
-    case 40: case 50:
-      rhog_ = TDatabase::ParamDB->P7; // rho of gas = rho2=rhomin
-      mug_  = TDatabase::ParamDB->P8; // mu of gas  = mu2 =mumin
-//    double density_ratio   = rhol_/rhog_;
-//    double viscosity_ratio = mul_/mug_;
-      break;
-
-    default:
-      break;
-  }
 }
 
-/** Update the vectors mu and rho with the phase fraction vector */
+/** Update the vectors mu and rho with the phase fraction vector,
+ * depending on the example number */
 void VOF_TwoPhase2D::update_field_vectors()
 {
-  this->rho_vector_ = this->phaseconvection2d_.get_solution();
-  this->mu_vector_ = this->phaseconvection2d_.get_solution();
-  BlockVector unity = this->rho_vector_; unity = 1;
-  this->rho_vector_.scale(this->rhol_-this->rhog_);
-  this->mu_vector_.scale(this->mul_-this->mug_);
-  this->rho_vector_.add_scaled(unity,this->rhog_);
-  this->mu_vector_.add_scaled(unity,this->mug_);
+  //At this point, rhol and mul = phase fraction
+  switch(example_number_)
+  {
+    case 10: case 20:
+    case 21: case 22:
+      this->rho_vector_ = this->rhol_;
+      this->mu_vector_ = this->mul_;
+      // note that in case of standard tnse2d, these values
+      // won't be used or read in the tnse2d object
+      break;
+    case 30: case 31:
+    case 32:
+      // here rho=1 and viscosity = phase field
+      this->rho_vector_ = this->unity_vector_;
+      break;
+    case 41:
+      // rayleigh taylor: visco = 1 and rho=phase field
+      this->mu_vector_ = this->unity_vector_;
+      break;
+    case 40: case 50:
+      // dambreak and 2-phase flows
+      this->rhog_ = TDatabase::ParamDB->P7; // rho of gas = rho2=rhomin
+      this->mug_  = TDatabase::ParamDB->P8; // mu of gas  = mu2 =mumin
+//    double density_ratio   = rhol_/rhog_;
+//    double viscosity_ratio = mul_/mug_;
+      this->rho_vector_.scale(this->rhol_-this->rhog_);
+      this->rho_vector_.add_scaled(this->unity_vector_,this->rhog_);
+      this->mu_vector_.scale(this->mul_-this->mug_);
+      this->mu_vector_.add_scaled(this->unity_vector_,this->mug_);
+      break;
+    default:
+      ErrThrow("Unknown example number");
+      break;
+  }
 }
 
 /** Write output of vectors in a file */
