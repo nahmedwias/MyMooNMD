bd3190c0eae2263fd3eaf31dd8458041d902844e
Najib Alia <Najib Alia@localhost>
2017-03-16 14:41:58 +0200

    Added a dummy example, compiled and tested.
    Constructor of matrices, vectors and TLinElastic2D object is so far ok.
    
    Next step: Continue constructing the objects and initializing the members.

--- user_projects/src/Examples/Example_TimeLinElastic2D.C
+++ user_projects/src/Examples/Example_TimeLinElastic2D.C
@@ -5,10 +5,51 @@
 #include <string>
 
 
+namespace test_tlinelastic // example number 0
+{
+ #include <0_Test_TLinElastic2D.h>
+}
+
 //=========================================
 Example_TimeLinElastic2D::Example_TimeLinElastic2D(
     const ParameterDatabase& user_input_parameter_db)
 : Example_NonStationary2D(user_input_parameter_db)
 {
+  int example_code = this->example_database["example"];
+
+  switch(example_code)
+    {
+      case 0:       // 0_Test_TLinElastic2D.h
+        /** output example name */
+        test_tlinelastic::ExampleFile();
+
+        /** exact_solution */
+        exact_solution.push_back( test_tlinelastic::ExactU1 );
+        exact_solution.push_back( test_tlinelastic::ExactU2 );
+
+        /** initial condition */
+        initialCOndtion.push_back(test_tlinelastic::InitialU1);
+        initialCOndtion.push_back(test_tlinelastic::InitialU2);
+        initialCOndtion.push_back(test_tlinelastic::InitialV1);
+        initialCOndtion.push_back(test_tlinelastic::InitialV2);
+
+        /** boundary condition */
+        boundary_conditions.push_back( test_tlinelastic::BoundCondition );
+
+        /** boundary values */
+        boundary_data.push_back( test_tlinelastic::BoundValueU1 );
+        boundary_data.push_back( test_tlinelastic::BoundValueU2 );
+
+        /** coefficients */
+        problem_coefficients = test_tlinelastic::Coefficients;
+
+        this->timeDependentRhs   =test_tlinelastic::rhs_depends_on_time;
+        this->timeDependentCoeffs=test_tlinelastic::coefficients_depend_on_time;
+
+        break;
+      default:
+        ErrThrow("Unknown example");
+    }
+
   cout << "CONSTRUCTOR OF EXAMPLE_TimeLinElastic2D OK!" << endl;
 }
