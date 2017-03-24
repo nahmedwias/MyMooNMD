cb2693648053ad951572bc50e284b6a3cc71320a
Najib Alia <Najib Alia@localhost>
2017-03-24 16:08:54 +0200

    Finished assemble routines for PB VMS for tnse2D.
    The localassembling object is also constructed for DISCTYPE = VMS_PROJECTION (as in LocalAssembling3D).

--- src/TimeDiscretization/assemble_routine_tnse2D_PBVMS.C
+++ src/TimeDiscretization/assemble_routine_tnse2D_PBVMS.C
@@ -275,10 +275,130 @@ void TimeNSType4VMS_ProjectionDD2D(double Mult, double *coeff,
 }
 
 //================================================================================
+/* ======================================================================
+ * Type 3, VMS_Projection, D(u):D(v), only nonlinear diagonal blocks
+ * Type 4, VMS_Projection, D(u):D(v), only nonlinear diagonal blocks
+ * ======================================================================*/
+void TimeNSType3_4NLVMS_ProjectionDD2D(double Mult, double *coeff,
+                double *param, double hK,
+                double **OrigValues, int *N_BaseFuncts,
+                double ***LocMatrices, double **LocRhs)
+{
+  double **MatrixA11, **MatrixA12, **MatrixA21;//**MatrixA13,
+  double **MatrixA22 ;//**MatrixA23, **MatrixA31, **MatrixA32;
+//  double **MatrixA33;
+  double val1,val2, val3, valu1, valu2;//, valu3;val4,
+  double **Matrix_tilde_G11, **Matrix_tilde_G22;//, **Matrix_tilde_G33;
+  double *Matrix11Row, *Matrix12Row, *Matrix21Row;//*Matrix13Row,
+  double *Matrix22Row;//, *Matrix23Row, *Matrix31Row, *Matrix32Row;
+//  double *Matrix33Row;
+  double ansatz10, ansatz01, ansatz00;//, ansatz001;  // double ansatz000;
+  double test00, test10, test01;//, test001;
+  double *Orig0, *Orig1, *Orig2, *Orig3;//, *Orig4;
+  int i,j,N_U, N_L;  // int N_P;
+  double c0, viscosity;
+  double u1, u2, mu;//u3
 
-//================================================================================
+  MatrixA11 = LocMatrices[0];
+  MatrixA12 = LocMatrices[1];
+//  MatrixA13 = LocMatrices[2];
+  MatrixA21 = LocMatrices[2];
+  MatrixA22 = LocMatrices[3];
+//  MatrixA23 = LocMatrices[5];
+//  MatrixA31 = LocMatrices[6];
+//  MatrixA32 = LocMatrices[7];
+//  MatrixA33 = LocMatrices[8];
+  Matrix_tilde_G11  = LocMatrices[4];
+  Matrix_tilde_G22  = LocMatrices[5];
+//  Matrix_tilde_G33  = LocMatrices[11];
 
-//================================================================================
+  N_U = N_BaseFuncts[0];
+  N_L = N_BaseFuncts[2];
 
+  Orig0 = OrigValues[0]; // u_x
+  Orig1 = OrigValues[1]; // u_y
+//  Orig2 = OrigValues[2]; // u_z
+  Orig2 = OrigValues[2]; // u
+  Orig3 = OrigValues[3]; // l
 
-//================================================================================
+  c0 = coeff[0]; // nu
+
+  u1 = param[0]; // u1old
+  u2 = param[1]; // u2old
+//  u3 = param[2]; // u3old
+
+//  double *x = &param[12];
+//  double *y = &param[13];
+//  double *z = &param[14];
+  double *u = &param[0];
+  double *gradu = &param[2];
+  double *uConv = &param[0];
+//  double *projection_space_label = &param[15];
+
+  mu = turbulentviscosity(hK, gradu, u, uConv);
+  viscosity = Mult*(mu/2.0+c0);
+
+  for(i=0;i<N_U;i++)
+  {
+    Matrix11Row = MatrixA11[i];
+    Matrix12Row = MatrixA12[i];
+//    Matrix13Row = MatrixA13[i];
+    Matrix21Row = MatrixA21[i];
+    Matrix22Row = MatrixA22[i];
+//    Matrix23Row = MatrixA23[i];
+//    Matrix31Row = MatrixA31[i];
+//    Matrix32Row = MatrixA32[i];
+//    Matrix33Row = MatrixA33[i];
+    test10 = viscosity*Orig0[i];
+    test01 = viscosity*Orig1[i];
+//    test001 = viscosity*Orig2[i];
+    test00 = Mult*Orig2[i];
+    valu1 = u1 * test00;
+    valu2 = u2 * test00;
+//    valu3 = u3 * test000;
+
+    for(j=0;j<N_U;j++)
+    {
+      ansatz10 = Orig0[j];
+      ansatz01 = Orig1[j];
+//      ansatz001 = Orig2[j];
+
+      //val1 = (u1*ansatz100+u2*ansatz010+u3*ansatz001)*test000;
+      val1 = valu1*ansatz10+valu2*ansatz01;//+valu3*ansatz001;
+
+      val2 = test10*ansatz10;
+      val3 = test01*ansatz01;
+//      val4 = test001*ansatz001;
+      val1 += val2+val3;//+val4;
+      Matrix11Row[j] += val2+val1;
+      Matrix12Row[j] += test01*ansatz10;
+//      Matrix13Row[j] += test001*ansatz100;
+      Matrix21Row[j] += test10*ansatz01;
+      Matrix22Row[j] += val3+val1;
+//      Matrix23Row[j] += test001*ansatz010;
+//      Matrix31Row[j] += test100*ansatz001;
+//      Matrix32Row[j] += test010*ansatz001;
+//      Matrix33Row[j] += val4+val1;
+    } // endfor j
+  } // endfor i
+
+  val2 = Mult * mu;
+  for(i=0;i<N_U;i++)
+  {
+     Matrix11Row = Matrix_tilde_G11[i];
+     Matrix22Row = Matrix_tilde_G22[i];
+//     Matrix33Row = Matrix_tilde_G33[i];
+     test10 = Orig0[i];
+     test01 = Orig1[i];
+//     test001 = Orig2[i];
+
+     for(j=0;j<N_L;j++)
+     {
+        ansatz00 = Orig3[j];
+        val1 = val2 * ansatz00;
+        Matrix11Row[j] -= val1 * test10;
+        Matrix22Row[j] -= val1 * test01;
+//        Matrix33Row[j] -= val1 * test001;
+     }
+  }
+}
