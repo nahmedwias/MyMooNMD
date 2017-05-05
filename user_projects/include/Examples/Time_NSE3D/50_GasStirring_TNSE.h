e66ba3ed2c56cd5822124e0cb9cb4c7968d5bd90
Najib Alia <Najib Alia@localhost>
2017-05-05 14:37:14 +0300

    Here is a good configuration to test gas injection?
    Take Circle2.GEO, with DRIFT_Z=1. Then (ref=3,n_cell_layers=1) or (2,5). First one is 77kdof, second 48kdof.
    
    Computes until ENDTIME=2.5s, with Dt=0.1, in 800s. With iterative solver.
    
    Next step= continue testing gas injection

--- user_projects/include/Examples/Time_NSE3D/50_GasStirring_TNSE.h
+++ user_projects/include/Examples/Time_NSE3D/50_GasStirring_TNSE.h
@@ -4,6 +4,14 @@
 // This is also called nu, or eps, it is equal
 // to 1/Reynolds_number and is dimensionless
 double DIMENSIONLESS_VISCOSITY;
+// coordinates of the plugs p1 and p2
+//  double p1x = -0.566,  p1y = +0.5;
+//  double p2x = -0.468,  p2y = -0.27;
+double p1x = 0,  p1y = 0;
+double p2x = 0,  p2y = 0;
+double p_radius = 0.1;
+double height = 2.5;
+double inflow_velocity = 0.05;
 
 void ExampleFile()
 {
@@ -61,12 +69,6 @@ void ExactP(double x, double y,  double z, double *values)
 // ========================================================================
 void BoundCondition(double x, double y, double z, BoundCond &cond)
 {
-  // coordinates of the plugs p1 and p2
-  //  double p1x = -0.566,  p1y = +0.5;
-  //  double p2x = -0.468,  p2y = -0.27;
-    double p1x = 0,  p1y = 0;
-    double p2x = 0,  p2y = 0;
-  double p_radius = 0.5;
   double in_plug1 = p_radius*p_radius-(x-p1x)*(x-p1x)-(y-p1y)*(y-p1y);
   double in_plug2 = p_radius*p_radius-(x-p2x)*(x-p2x)-(y-p2y)*(y-p2y);
 
@@ -95,12 +97,6 @@ void U2BoundValue(double x, double y, double z, double &value)
 // value of boundary condition
 void U3BoundValue(double x, double y, double z, double &value)
 {
-  // coordinates of the plugs p1 and p2
-  //  double p1x = -0.566,  p1y = +0.5;
-  //  double p2x = -0.468,  p2y = -0.27;
-    double p1x = 0,  p1y = 0;
-    double p2x = 0,  p2y = 0;
-  double p_radius = 0.5;
   double in_plug1 = p_radius*p_radius-(x-p1x)*(x-p1x)-(y-p1y)*(y-p1y);
   double in_plug2 = p_radius*p_radius-(x-p2x)*(x-p2x)-(y-p2y)*(y-p2y);
 
@@ -110,7 +106,7 @@ void U3BoundValue(double x, double y, double z, double &value)
 //    start = 1;
 
   if ( (z == 0) && (in_plug1 >=0 || in_plug2 >= 0) )
-    value = 0.01; //gas inflow
+    value = inflow_velocity; //gas inflow
   else
     value = 0;
 }
