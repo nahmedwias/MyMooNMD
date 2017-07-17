1fe5b30bcb60032765beb59c22667f096073bd8f
Najib Alia <Najib Alia@localhost>
2017-07-17 12:33:59 +0300

    FOUND ONE MISTAKE IN BRACKBILL ET AL!!!
    In this commit, tried to obtain pressure 1.1805 as in Brackbill et al by playing around with the constants in the right hand side. Ended up with constant = <Rho> * Rho_max * 6.3.., this 6.3 looks like a 2*pi.
    
    Looking on the internet, the Laplace formulas was found several times to be Dp = 2*s/R!!! In Brackbill, the 2 is missing.
    
    The benchmark is not validated, but at least the formula and the correct pressure are corrected now.

--- src/AssembleRoutines/TNSE2D_FixPo.C
+++ src/AssembleRoutines/TNSE2D_FixPo.C
@@ -5823,7 +5823,7 @@ double ***LocMatrices, double **LocRhs)
 // surface tension coefficients
   double tau = TDatabase::ParamDB->P9;
   double surfacetension = tau*kappa; //divided by average in interface
-  surfacetension /= (1); // divided by Phi average and Rho jump
+  surfacetension /= (750*1000*6.3449045); // divided by Phi average and Rho jump
 //  cout << surfacetension << " " ;
 //  cout << Phi_xx << " " << Phi_yy << " ";
 //  cout << Phi_xy << " ";
@@ -5832,8 +5832,8 @@ double ***LocMatrices, double **LocRhs)
   {
     test00 = Orig0[i];
 
-    Rhs1[i] += R*Mult*test00*c1 + surfacetension*Phi_x*test00*R/750;//Phi_x;
-    Rhs2[i] += R*Mult*test00*c2 + surfacetension*Phi_y*test00*R/750;//Phi_y;
+    Rhs1[i] += R*Mult*test00*c1 + surfacetension*Phi_x*test00/1;//Phi_x;
+    Rhs2[i] += R*Mult*test00*c2 + surfacetension*Phi_y*test00/1;//Phi_y;
 //    cout <<  Rhs1[i] << " " <<  Rhs2[i] << " ";
   }                              // endfor i
 }
