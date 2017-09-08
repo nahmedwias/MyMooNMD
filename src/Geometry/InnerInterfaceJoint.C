dab8a7cf42aed08703b0e0d4d7adebaf3770773e
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2017-09-08 15:45:49 +0200

    added a few getter methods for NSE3D. Updated TInnerInterfaceJoints for 3D

--- src/Geometry/InnerInterfaceJoint.C
+++ src/Geometry/InnerInterfaceJoint.C
@@ -24,18 +24,31 @@ TInnerInterfaceJoint::TInnerInterfaceJoint(
   NeibGlobalCellNo[1]=neighb1->GetCellIndex();
   SubDomainsID[0]=neighb0->GetReference_ID();
   SubDomainsID[1]=neighb1->GetReference_ID();
-  children[0] = NULL;
-  children[1] = NULL;
+  children[0] = nullptr;
+  children[1] = nullptr;
+  //for 3D
+  children[2] = nullptr;
+  children[3] = nullptr;
   
-  // find coordinates of edge end points 
-  // it does not matter which neighbor we take here 
-  // first find which edge of neighb0 is this joint
-  
+  //for 2D starting point and vector pointing to other point of the edge
   Xstart = -4711;
   Ystart = -4711;
   delX = -4711;
   delY = -4711;
   
+  //for 3D
+  //number of vertices per face
+  N_vert = -4711;
+  //array of length N_vert containing x-,y- and z-component
+  X = nullptr;
+  Y = nullptr;
+  Z = nullptr;
+  //which local face of the cell is this interface
+  //which neighboring cell is used depends on the ID given
+  //in GetInnerInterface in auxiliaryFunctions.C
+  local_face = -4711;
+
+  dim = -4711;
 }
 
 TInnerInterfaceJoint::TInnerInterfaceJoint(TBaseCell *neighb0): 
@@ -46,13 +59,31 @@ TInnerInterfaceJoint::TInnerInterfaceJoint(TBaseCell *neighb0):
   NeibGlobalCellNo[1]=-1;
   SubDomainsID[0]=neighb0->GetReference_ID();
   SubDomainsID[1]=-1;
-  children[0] = NULL;
-  children[1] = NULL;
+  children[0] = nullptr;
+  children[1] = nullptr;
+  //for 3D
+  children[2] = nullptr;
+  children[3] = nullptr;
   
+  //for 2D starting point and vector pointing to other point of the edge
   Xstart = -4711;
   Ystart = -4711;
   delX = -4711;
   delY = -4711;
+
+  //for 3D
+  //number of vertices per face
+  N_vert = -4711;
+  //array of length N_vert containing x-,y- and z-component
+  X = nullptr;
+  Y = nullptr;
+  Z = nullptr;
+  //which local face of the cell is this interface
+  //which neighboring cell is used depends on the ID given
+  //in GetInnerInterface in auxiliaryFunctions.C
+  local_face = -4711;
+
+  dim = -4711;
 }
 
 
@@ -64,20 +95,27 @@ TJoint *TInnerInterfaceJoint::NewInst(double newT_0, double newT_1, TBaseCell *M
 
 TJoint *TInnerInterfaceJoint::NewInst()
 {
-  return new TInnerInterfaceJoint(NULL);
+  return new TInnerInterfaceJoint(nullptr);
 }
 
 /** Remember which joints are children of this joint */
 void TInnerInterfaceJoint::SetChild(TInnerInterfaceJoint *child)
 {
-  if(children[0] == NULL)
+  if(children[0] == nullptr)
     children[0] = child;
-  else if(children[1] == NULL)
+  else if(children[1] == nullptr)
     children[1] = child;
-  // if two children are already set, then nothing happens,
-  if(children[0]!=child && children[1]!=child)
+  else if(children[2] == nullptr && dim==3)
+      children[2] = child;
+  else if(children[3] == nullptr && dim==3)
+      children[3] = child;
+  // if two (in 2D) or four (in3D) children are already set, then nothing
+  // happens,
+  if((children[0] != child && children[1] != child &&
+      children[2] != child && children[3] != child && dim==3) ||
+     (children[0] != child && children[1] != child && dim==2))
   {
-    ErrMsg("ERROR, TInnerInterfaceJoint::SetChild");
+    ErrThrow("TInnerInterfaceJoint::SetChild");
   }
 }
 
@@ -86,10 +124,11 @@ TInnerInterfaceJoint *TInnerInterfaceJoint::GetChild(int child) const
 {
   if(child == 0 || child == 1)
     return children[child];
+  else if((child == 2 || child == 3) && dim == 3)
+    return children[child];
   else
   {
-    ErrMsg("ERROR, TInnerInterfaceJoint::GetChild, no such child available");
-    exit(-4711);
+    ErrThrow("ERROR, TInnerInterfaceJoint::GetChild, no such child available");
   }
 }
 
@@ -102,8 +141,7 @@ TBaseCell *TInnerInterfaceJoint::GetNeighbour(int i) const
     return Neighb0;
   else
   {
-    ErrMsg("TInnerInterfaceJoint::GetNeighbour: only two neighbors " <<i);
-    exit(0);
+    ErrThrow("TInnerInterfaceJoint::GetNeighbour: only two neighbors ", i);
   }
 }
 
@@ -116,6 +154,8 @@ void TInnerInterfaceJoint::SetParams(double xstart, double ystart,
   Ystart = ystart;
   delX = delx;
   delY = dely;
+  dim = 2;
+  N_vert = 2;
 }
 /** get the coordinates of the start point and the vector pointing to the     
     second point */
@@ -128,9 +168,64 @@ void TInnerInterfaceJoint::GetParams(double &xstart, double &ystart,
   dely = delY;
 }
 
+void TInnerInterfaceJoint::SetParams(double *x, double *y, double *z, 
+                                     int n_vert)
+{
+  N_vert = n_vert;
+
+  X = new double[n_vert];
+  Y = new double[n_vert];
+  Z = new double[n_vert];
+
+  for(unsigned int i_vert=0; i_vert < N_vert; i_vert++){
+    X[i_vert] = x[i_vert];
+    Y[i_vert] = y[i_vert];
+    Z[i_vert] = z[i_vert];
+  }
+
+  dim = 3;
+}
+
+void TInnerInterfaceJoint::GetParams(double *x, double *y, double *z) const
+{
+  for(unsigned int i_vert=0; i_vert < N_vert; i_vert++){
+    x[i_vert] = X[i_vert];
+    y[i_vert] = Y[i_vert];
+    z[i_vert] = Z[i_vert];
+  }
+}
+
 double TInnerInterfaceJoint::GetLength() const
 {
   return sqrt(delX*delX + delY*delY);
+}
+
+double TInnerInterfaceJoint::GetArea() const
+{
+  if(N_vert == 3){
+    //using A=0.5*|X x Y|
+    double x1 = X[1]-X[0];
+    double x2 = Y[1]-Y[0];
+    double x3 = Z[1]-Z[0];
+    double y1 = X[2]-X[0];
+    double y2 = Y[2]-Y[0];
+    double y3 = Z[2]-Z[0];
+    return 0.5*sqrt(pow(x2*y3-x3*y2,2) + pow(x3*y1-x1*y3,2) + pow(x1*y2-x2*y1,2));
+  }
+  if(N_vert == 4){
+  //using A=0.5*sqrt(|e|^2*|f|^2-(e,f)^2)
+  double norm_e = pow((X[0]-X[2]),2)
+                 +pow((Y[0]-Y[2]),2)
+                 +pow((Z[0]-Z[2]),2);
+  double norm_f = pow((X[1]-X[3]),2)
+                 +pow((Y[1]-Y[3]),2)
+                 +pow((Z[1]-Z[3]),2);
+  double ef = (X[0]-X[2])*(X[1]-X[3])
+             +(Y[0]-Y[2])*(Y[1]-Y[3])
+             +(Z[0]-Z[2])*(Z[1]-Z[3]);
+  return 0.5*sqrt(norm_e*norm_f-ef*ef);
+  }
+  ErrThrow("N_vert = ", N_vert);
 }
 
 /** the unit normal of this edge */
@@ -140,13 +235,46 @@ void TInnerInterfaceJoint::GetNormal(double &nx, double &ny) const
   nx =  delY/length;
   ny = -delX/length;
 }
-  
+
+/** the unit normal of this face */
+void TInnerInterfaceJoint::GetNormal(double &nx, double &ny, double &nz) const
+{
+  nx =  ((Y[1]-Y[0])*(Z[N_vert-1]-Z[1]) - (Z[1]-Z[0])*(Y[N_vert-1]-Y[1]));
+  ny =  ((Z[1]-Z[0])*(X[N_vert-1]-X[1]) - (X[1]-X[0])*(Z[N_vert-1]-Z[1]));
+  nz =  ((X[1]-X[0])*(Y[N_vert-1]-Y[1]) - (Y[1]-Y[0])*(X[N_vert-1]-X[1]));
+  double length = sqrt(nx*nx + ny*ny + nz*nz);
+  nx = nx/length;
+  ny = ny/length;
+  nz = nz/length;
+}
+
 /** the unit tangential of this edge */
 void TInnerInterfaceJoint::GetTangent(double &tx, double &ty) const
 {
   double length = sqrt(delX*delX + delY*delY);
   tx = delX/length;
   ty = delY/length;
+}
+/** the unit tangentials of this face */
+void TInnerInterfaceJoint::GetTangent(double &tx, double &ty, double &tz,
+                                      double &tx2, double &ty2, double &tz2) const
+{
+  // first tangent
+  double length = sqrt((X[1]-X[0])*(X[1]-X[0])
+                     + (Y[1]-Y[0])*(Y[1]-Y[0])
+                     + (Z[1]-Z[0])*(Z[1]-Z[0]));
+  tx = (X[1]-X[0])/length;
+  ty = (Y[1]-Y[0])/length;
+  tz = (Z[1]-Z[0])/length;
+  // second tangent computed via cross-product with normal
+  double nx,ny,nz;
+  this->GetNormal(nx,ny,nz);
+  length = sqrt((ty*nz - tz*ny)*(ty*nz - tz*ny)
+              + (tz*nx - tx*nz)*(tz*nx - tx*nz)
+              + (tx*ny - ty*nx)*(tx*ny - ty*nx));
+  tx2 = (ty*nz - tz*ny)/length;
+  ty2 = (tz*nx - tx*nz)/length;
+  tz2 = (tx*ny - ty*nx)/length;
 }
 
 /** get the index of this joint in given neighbor */
@@ -156,11 +284,7 @@ int TInnerInterfaceJoint::GetIndexInNeighbor(const TBaseCell*const neigh) const
     return IndexInNeighbor[0];
   else if(neigh == Neighb1)
     return IndexInNeighbor[1];
-  else
-  {
-    ErrMsg("ERROR, TInnerInterfaceJoint::GetIndexInNeighbor !!!!!!!!");
-    exit(-4711);
-  }
+  ErrThrow("TInnerInterfaceJoint::GetIndexInNeighbor !!!!!!!!");
 }
 
 /** set the index of this joint in given neighbor */
@@ -170,9 +294,36 @@ void TInnerInterfaceJoint::SetIndexInNeighbor(TBaseCell *neigh, int index)
     IndexInNeighbor[0] = index;
   else if(neigh == Neighb1)
     IndexInNeighbor[1] = index;
-  else
+    ErrThrow("TInnerInterfaceJoint::SetIndexInNeighbor !!!!!!!!");
+}
+
+void TInnerInterfaceJoint::PrintInfo() const
+{
+  if(dim == 3)
   {
-    ErrMsg("ERROR, TInnerInterfaceJoint::SetIndexInNeighbor !!!!!!!!");
-    exit(-4711);
+    Output::print("This InnerInterface has ", N_vert, " vertices");
+    for(unsigned int iVert = 0; iVert < N_vert; iVert++)
+    {
+      Output::print("Vertex ", iVert, ": (", X[iVert], ",", Y[iVert], ",",
+                    Z[iVert], ")");
+    }
+    Output::print("Index of this face in the first neighbor: ",
+                  IndexInNeighbor[0]);
+    Output::print("Index of this face in the second neighbor: ",
+                  IndexInNeighbor[1]);
+    Output::print("Local face: ", local_face);
   }
+  else if(dim == 2)
+  {
+    Output::print("This InnerInterface has ", N_vert, " vertices");
+    Output::print("Vertex 1: (", Xstart, ",", Ystart, ")");
+    Output::print("Vertex 2: (", Xstart+delX, ",", Ystart+delY, ")");
+    Output::print("Index of this face in the first neighbor: ",
+                  IndexInNeighbor[0]);
+    Output::print("Index of this face in the second neighbor: ",
+                  IndexInNeighbor[1]);
+    Output::print("Local face: ", local_face);
+  }
+  else
+    Output::print("dim = ", dim, ". dimension should be 2D or 3D");
 }
