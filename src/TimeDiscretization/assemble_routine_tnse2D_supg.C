89302801f4adef83ff81c33addc38c10696cabce
ahmed <ahmed@wias-berlin.de>
2017-10-19 12:13:56 +0200

    update: IMEX scheme, nstype 4: fixed some bugs in the NSTYPE 4, checked for the mixing layer problem only at the moment

--- src/TimeDiscretization/assemble_routine_tnse2D_supg.C
+++ src/TimeDiscretization/assemble_routine_tnse2D_supg.C
@@ -647,10 +647,10 @@ void TimeNSType14RHSSUPG(double Mult, double* coeff, double* param, double hK,
   double c1=coeff[1];
   double c2=coeff[2];
 
-  double u1=param[0];
-  double u2=param[1];
   double u1_combined_old_time = param[2];
   double u2_combined_old_time = param[3];
+  double u1sigma=param[4];
+  double u2sigma=param[5];
 
   // stabilization parameters
   double tau_m;
@@ -658,7 +658,7 @@ void TimeNSType14RHSSUPG(double Mult, double* coeff, double* param, double hK,
   {
     double t = TDatabase::TimeDB->CURRENTTIMESTEPLENGTH;
     tau_m = 4./(t*t);
-    double u = sqrt(u1*u1+u2*u2);
+    double u = sqrt(u1sigma*u1sigma+u2sigma*u2sigma);
     tau_m += 4.*u/(hK*hK);
     double hp4 = pow(hK,4);
     tau_m += 32.*c0*c0/hp4;
@@ -682,7 +682,7 @@ void TimeNSType14RHSSUPG(double Mult, double* coeff, double* param, double hK,
     test01 = Orig1[i];
     test00 = Orig2[i];
     
-    double ugrad = tau_m * (u1*test10+u2*test01);
+    double ugrad = tau_m * (u1sigma*test10+u2sigma*test01);
     // right hand side 
     // standard terms 
     Rhs1[i] += Mult*(test00+ugrad)*c1;
@@ -707,140 +707,123 @@ void TimeNSType14RHSSUPG(double Mult, double* coeff, double* param, double hK,
 void TimeNSType4SUPGExtr(double Mult, double* coeff, double* param, double hK, 
       double** OrigValues, int* N_BaseFuncts, double*** LocMatrices, double** LocRhs)
 {
-  double **sqMatrixA11 = LocMatrices[0];
-  double **sqMatrixA12 = LocMatrices[1];
-  double **sqMatrixA21 = LocMatrices[2];
-  double **sqMatrixA22 = LocMatrices[3];
+  double **MatrixA11 = LocMatrices[0];
+  double **MatrixA12 = LocMatrices[1];
+  double **MatrixA21 = LocMatrices[2];
+  double **MatrixA22 = LocMatrices[3];
   // weighted mass matrix
-  double **sqMatrixM = LocMatrices[4];
+  double **MatrixM = LocMatrices[4];
   
-  double **reMatrixB1 = LocMatrices[5];
-  double **reMatrixB2 = LocMatrices[6];
-  double **reMatrixB1T= LocMatrices[7];
-  double **reMatrixB2T= LocMatrices[8];
+  double **MatrixB1 = LocMatrices[5];
+  double **MatrixB2 = LocMatrices[6];
+  double **MatrixB1T= LocMatrices[7];
+  double **MatrixB2T= LocMatrices[8];
   
   // velocity part 
-  double *rhs1 = LocRhs[0];
-  double *rhs2 = LocRhs[1];
+  double *Rhs1 = LocRhs[0];
+  double *Rhs2 = LocRhs[1];
   
-  double *uv_xorig = OrigValues[0];
-  double *uv_yorig = OrigValues[1];
-  double *uv_orig = OrigValues[2]; 
+  double *Orig0 = OrigValues[0]; // u_x
+  double *Orig1 = OrigValues[1]; // u_y
+  double *Orig2 = OrigValues[2]; // u
   
-  double *pq_orig = OrigValues[3]; 
-  double *pq_xorig = OrigValues[4];
-  double *pq_yorig = OrigValues[5];
+  double *Orig3 = OrigValues[3]; // p
+  double *Orig4 = OrigValues[4]; // p_x
+  double *Orig5 = OrigValues[5]; // p_y
   
-  double *uxx_orig = OrigValues[6];
-  double *uyy_orig = OrigValues[7];
+  double *Orig6 = OrigValues[6]; // u_xx
+  double *Orig7 = OrigValues[7]; // u_yy
   
   int N_U = N_BaseFuncts[0];
   int N_P = N_BaseFuncts[1];
-  // problem parameters
-  double nu=coeff[0];
-  double f1=coeff[1];
-  double f2=coeff[2];
   
-  // solution from previous iterations
-  // or the extrapolated solution from previous time step
-  // in the case of extrapolations also in the nonlinear 
-  // term on the left hand side of NS-equation
-  double u1=param[0];
-  double u2=param[1];
-  // solution from the previous time step 
-  // this can also be constant or linearly extrapolated
-  double u1_extr=param[2];
-  double u2_extr=param[3];
-  
-  double test10, test01, test00, val;  
+  double c0=coeff[0];
+  double c1=coeff[1];
+  double c2=coeff[2];
+
+  double u1sigma=param[4];
+  double u2sigma=param[5];
+
+  double test10, test01, test00;  
   double ansatz10, ansatz01, ansatz00, ansatz20, ansatz02;
-  double tau_m =  hK*hK*TDatabase::ParamDB->DELTA0;
-  double tau_c =  TDatabase::ParamDB->DELTA1;
+  double tau_m, tau_c;
+  tau_m =  hK*hK*TDatabase::ParamDB->DELTA0;
+  tau_c =  TDatabase::ParamDB->DELTA1;
   
   
   for(int i=0; i<N_U; ++i)
   {
-    test10 = uv_xorig[i];
-    test01 = uv_yorig[i];
-    test00 = uv_orig[i];
+    test10 = Orig0[i];
+    test01 = Orig1[i];
+    test00 = Orig2[i];
     
-    double tau_m_uptsgradv = tau_m * (u1_extr*test10+u2_extr*test01);
+    double usigmagradv = tau_m * (u1sigma*test10+u2sigma*test01);
     // right hand side 
     // standard terms 
-    rhs1[i] += Mult*(test00+tau_m_uptsgradv)*f1;
-    rhs2[i] += Mult*(test00+tau_m_uptsgradv)*f2;
+    Rhs1[i] += Mult*(test00+usigmagradv)*c1;
+    Rhs2[i] += Mult*(test00+usigmagradv)*c2;
     // velocity-velocity blocks
     for(int j=0; j<N_U; ++j)
     {
-      ansatz10 = uv_xorig[j];
-      ansatz01 = uv_yorig[j];
-      ansatz00 = uv_orig[j];
+      ansatz10 = Orig0[j];
+      ansatz01 = Orig1[j];
+      ansatz00 = Orig2[j];
       
-      ansatz20 = uxx_orig[j];
-      ansatz02 = uyy_orig[j];
+      ansatz20 = Orig6[j];
+      ansatz02 = Orig7[j];
       
-      double ugradu = u1*ansatz10 + u2*ansatz01;
-      // viscous and convection term
-      val = nu*(test10*ansatz10 + test01*ansatz01)
-            + ugradu * test00; 
+      double usigmagradu = u1sigma*ansatz10 + u2sigma*ansatz01;
+      
+      // Galerkin part
+      double val  = c0*(test10*ansatz10 + test01*ansatz01) // diffusion term
+                     + usigmagradu*test00; // convective term
       // supg contribution
-      val += (-nu*(ansatz20 + ansatz02) + ugradu) * tau_m_uptsgradv;
-      // grad-div contribution
-      val += tau_c*test10*ansatz10;
-      sqMatrixA11[i][j] += Mult * val;
+      val +=  (-c0*(ansatz20 + ansatz02) + usigmagradu ) *usigmagradv;
+      // grad div contribution
+      MatrixA11[i][j] += Mult * (val + tau_c*test10*ansatz10); // A11 block
+      MatrixA22[i][j] += Mult * (val + tau_c*test01*ansatz01); // A22 block
       
       val = tau_c * test10*ansatz01;
-      sqMatrixA12[i][j] += Mult * val; 
+      MatrixA12[i][j] += Mult * val; // A12 block
       
       val = tau_c * test01*ansatz10;
-      sqMatrixA21[i][j] += Mult * val; 
+      MatrixA21[i][j] += Mult * val; // A21 block 
       
-      // viscous term 
-      val = nu*(test10*ansatz10 + test01*ansatz01)
-            + ugradu * test00; 
-      // supg contribution
-      val += (-nu*(ansatz20 + ansatz02) + ugradu) * tau_m_uptsgradv;
-      // grad-div contribution
-      val += tau_c*test01*ansatz01;
-      sqMatrixA22[i][j] += Mult * val;
-      
-      // mass matrix 
-      val = ansatz00 * (test00 + tau_m_uptsgradv);
-      sqMatrixM[i][j] += Mult * val; 
-    }
+      // weighted mass matrix
+      val = ansatz00 * (test00 + usigmagradv);
+      MatrixM[i][j] += Mult * val;      
+    }// endfor j<N_U
     
-    // pressure (ansatz) velocity(test) blocks
     for(int j=0; j<N_P; ++j)
     {
-      ansatz00 = pq_orig[j];
-      ansatz10 = pq_xorig[j];
-      ansatz01 = pq_yorig[j];
+      // pressure ansatz functions
+      ansatz00 = Orig3[j];
+      ansatz10 = Orig4[j];
+      ansatz01 = Orig5[j];
       
-      val = -ansatz00*test10;
-      val += ansatz10 * tau_m_uptsgradv;
-      reMatrixB1T[i][j] += Mult * val;
+      // pressure term
+      MatrixB1T[i][j] += Mult * (-ansatz00 * test10 + ansatz10 * usigmagradv);
       
-      val = -ansatz00*test01;
-      val += ansatz01*tau_m_uptsgradv;
-      reMatrixB2T[i][j] += Mult * val;
+      MatrixB2T[i][j] += Mult * (-ansatz00 * test01 + ansatz01 * usigmagradv);
     }
   }
   
-  // velocity(ansatz) pressure (test) blocks
+  // pressure test functions
   for(int i=0; i<N_P; ++i)
   {
     // pressure test
-    test00 = pq_orig[i];
+    test00 = Orig3[i];
+
+    // velocity-pressure block
     for(int j=0;j<N_U;j++)
     {
-      ansatz10 = uv_xorig[j];
-      ansatz01 = uv_yorig[j];
+      // velocity ansatz functions
+      ansatz10 = Orig0[j];
+      ansatz01 = Orig1[j];
 
-      val = -test00*ansatz10;
-      reMatrixB1[i][j] += Mult * val;
-      
-      val = -test00*ansatz01;
-      reMatrixB2[i][j] += Mult * val;
+      // divergence constraint
+      MatrixB1[i][j] +=Mult*(-test00*ansatz10);
+      MatrixB2[i][j] += Mult*(-test00*ansatz01);
     }    
   }
 }
@@ -849,69 +832,97 @@ void TimeNSType4SUPGExtr(double Mult, double* coeff, double* param, double hK,
 void TimeNSType4NLSUPGExtr(double Mult, double* coeff, double* param, double hK, 
  double** OrigValues, int* N_BaseFuncts, double*** LocMatrices, double** LocRhs)
 {
-  double **sqMatrixA11 = LocMatrices[0];
-  double **sqMatrixA22 = LocMatrices[1];
+  double **MatrixA11 = LocMatrices[0];
+  double **MatrixA22 = LocMatrices[1];
+  // weighted mass matrix
+  double **MatrixM = LocMatrices[2];
   
-  double *uv_xorig= OrigValues[0];
-  double *uv_yorig= OrigValues[1];
-  double *uv_orig = OrigValues[2];
-  double *uxx_orig= OrigValues[6];
-  double *uyy_orig= OrigValues[7];
+  double **MatrixB1T = LocMatrices[3];
+  double **MatrixB2T = LocMatrices[4];
   
-  // problem coefficients
-  double nu = coeff[0];
+  double *Rhs1 = LocRhs[0];
+  double *Rhs2 = LocRhs[1];
   
-  // solution from previous iterations
-  // or the extrapolated solution from previous time step
-  // in the case of extrapolations also in the nonlinear 
-  // term on the left hand side of NS-equation
-  double u1=param[0];
-  double u2=param[1];
-  // solution from the previous time step 
-  // this can also be constant or linearly extrapolated
-  double u1_extr=param[2];
-  double u2_extr=param[3];
+  double *Orig0 = OrigValues[0]; // u_x
+  double *Orig1 = OrigValues[1]; // u_y
+  double *Orig2 = OrigValues[2]; // u
   
-  double test10, test01, test00, val;
+  double *Orig3 = OrigValues[3]; // p
+  double *Orig4 = OrigValues[4]; // p_x
+  double *Orig5 = OrigValues[5]; // p_y
+  
+  double *Orig6 = OrigValues[6]; // u_xx
+  double *Orig7 = OrigValues[7]; // u_yy
+  
+  int N_U = N_BaseFuncts[0];
+  int N_P = N_BaseFuncts[1];
+  
+  double c0=coeff[0];
+  double c1=coeff[1];
+  double c2=coeff[2];
+
+  double u1sigma=param[4];
+  double u2sigma=param[5];
+
+  double test10, test01, test00;  
   double ansatz10, ansatz01, ansatz00, ansatz20, ansatz02;
-  double tau_m = TDatabase::ParamDB->DELTA0*hK*hK;
+  // initially for the test case 
+  double tau_m =  TDatabase::ParamDB->DELTA0*hK*hK;  
   double tau_c = TDatabase::ParamDB->DELTA1;
-  int n_u = N_BaseFuncts[0];
-  for(int i=0; i<n_u; ++i)
+  
+  double *Matrix11Row, *Matrix12Row, *Matrix21Row, *Matrix22Row;
+  for(int i=0; i<N_U; ++i)
   {
-    test10 = uv_xorig[i];
-    test01 = uv_yorig[i];
-    test00 = uv_orig[i];
+    Matrix11Row = MatrixA11[i];
+    Matrix22Row = MatrixA22[i];
+    test10 = Orig0[i];
+    test01 = Orig1[i];
+    test00 = Orig2[i];
     
-    double tau_m_uptsgradv = tau_m * (u1_extr*test10 + u2_extr*test01);
-    
-    // velocity-velocity block
-    for(int j=0; j<n_u; ++j)
+    double usigmagradv = tau_m * (u1sigma*test10+u2sigma*test01);
+    // right hand side 
+    Rhs1[i] += Mult*(test00+usigmagradv)*c1;
+    Rhs2[i] += Mult*(test00+usigmagradv)*c2;
+    // velocity-velocity blocks
+    for(int j=0; j<N_U; ++j)
     {
-      ansatz10 = uv_xorig[j];
-      ansatz01 = uv_yorig[j];
-      ansatz00 = uv_orig[j];
-      ansatz20 = uxx_orig[j];
-      ansatz02 = uyy_orig[j];
+      ansatz10 = Orig0[j];
+      ansatz01 = Orig1[j];
+      ansatz00 = Orig2[j];
       
-      double ugradu = u1*ansatz10 + u2*ansatz01;
-      // viscous and convection term
-      val = nu*(test10*ansatz10 + test01*ansatz01)
-            + ugradu * test00; 
-      // supg contribution
-      val += (-nu*(ansatz20 + ansatz02) + ugradu) * tau_m_uptsgradv;
-      // grad-div contribution
-      val += tau_c*test10*ansatz10;
-      sqMatrixA11[i][j] += Mult * val;
+      ansatz20 = Orig6[j];
+      ansatz02 = Orig7[j];
       
-      // viscous term 
-      val = nu*(test10*ansatz10 + test01*ansatz01)
-            + ugradu * test00; 
+      double usigmagradu = u1sigma*ansatz10 + u2sigma*ansatz01;
+      // Galerkin part
+      double val  = c0*(test10*ansatz10 + test01*ansatz01) // diffusion term
+                     + usigmagradu*test00; // convective term
       // supg contribution
-      val += (-nu*(ansatz20 + ansatz02) + ugradu) * tau_m_uptsgradv;
-      // grad-div contribution
-      val += tau_c*test01*ansatz01;
-      sqMatrixA22[i][j] += Mult * val;
+      val +=  (-c0*(ansatz20 + ansatz02) + usigmagradu )*usigmagradv;
+      // grad div contribution
+      Matrix11Row[j] += Mult * (val + tau_c*test10*ansatz10); // A11 block
+      Matrix22Row[j] += Mult * (val + tau_c*test01*ansatz01); // A22 block
+
+      // weighted mass matrix
+      val = ansatz00 * (test00 + usigmagradv);
+      MatrixM[i][j] += Mult * val;      
+    }// endfor j<N_U
+    
+    for(int j=0; j<N_P; ++j)
+    {
+      // pressure ansatz functions
+      ansatz00 = Orig3[j];
+      ansatz10 = Orig4[j];
+      ansatz01 = Orig5[j];
+      
+      // pressure term
+      double val  = -ansatz00 * test10; // Galerkin term
+      val +=  ansatz10 * usigmagradv; // SUPG term (grad p, u grad v)
+      MatrixB1T[i][j] += Mult * val;
+      
+      val  = -ansatz00 * test01; // Galerkin term
+      val +=  ansatz01 * usigmagradv; // SUPG term (grad p, u grad v)
+      MatrixB2T[i][j] += Mult*val;
     }
   }
 }
@@ -919,103 +930,35 @@ void TimeNSType4NLSUPGExtr(double Mult, double* coeff, double* param, double hK,
 void TimeNSType4RHSSUPGExtr(double Mult, double* coeff, double* param, double hK, 
  double** OrigValues, int* N_BaseFuncts, double*** LocMatrices, double** LocRhs)
 {
-  double **sqMatrixM = LocMatrices[0];
-  double **reMatrixB1 = LocMatrices[1];
-  double **reMatrixB2 = LocMatrices[2];
-  double **reMatrixB1T= LocMatrices[3];
-  double **reMatrixB2T= LocMatrices[4];
+  double *Rhs1 = LocRhs[0];
+  double *Rhs2 = LocRhs[1];
   
-  double *rhs1 = LocRhs[0];
-  double *rhs2 = LocRhs[1];
+  double *Orig0 = OrigValues[0]; // u_x
+  double *Orig1 = OrigValues[1]; // u_y
+  double *Orig2 = OrigValues[2]; // u
   
-  double *uv_xorig = OrigValues[0]; 
-  double *uv_yorig = OrigValues[1]; 
-  double *uv_orig = OrigValues[2]; 
-  
-  double *pq_orig = OrigValues[3]; 
-  double *pq_xorig = OrigValues[4];
-  double *pq_yorig = OrigValues[5];
-  
-  int N_U = N_BaseFuncts[0];
-  int N_P = N_BaseFuncts[1];
-  
-  // problem coefficients
-  double nu = coeff[0];
-  double f1 = coeff[1];
-  double f2 = coeff[2];
+  double c0=coeff[0];
+  double c1=coeff[1];
+  double c2=coeff[2];
 
-  // solution from previous iterations
-  // or the extrapolated solution from previous time step
-  // in the case of extrapolations also in the nonlinear 
-  // term on the left hand side of NS-equation
-  double u1=param[0];
-  double u2=param[1];
-  // solution from the previous time step 
-  // this can also be constant or linearly extrapolated
-  double u1_extr=param[2];
-  double u2_extr=param[3];
+  double u1sigma=param[4];
+  double u2sigma=param[5];
+
+  double tau_m =  TDatabase::ParamDB->DELTA0*hK*hK;
   
   double test10, test01, test00;  
-  double ansatz10, ansatz01, ansatz00, ansatz20, ansatz02;
-  double tau_m, tau_c;
-  tau_m =  hK*hK*TDatabase::ParamDB->DELTA0;
-  tau_c =  TDatabase::ParamDB->DELTA1;
-  
-  double val;
+  int N_U = N_BaseFuncts[0];
   for(int i=0; i<N_U; ++i)
   {
-    test10 = uv_xorig[i];
-    test01 = uv_yorig[i];
-    test00 = uv_orig[i];
+    test10 = Orig0[i];
+    test01 = Orig1[i];
+    test00 = Orig2[i];
     
-    double tau_m_uptsgradv = tau_m * (u1_extr*test10+u2_extr*test01);
-    // right-hand side Galerkin and SUPG contribution
-    rhs1[i] += Mult*(test00+tau_m_uptsgradv)*f1;
-    rhs2[i] += Mult*(test00+tau_m_uptsgradv)*f2;
-
-    for(int j=0; j<N_U; ++j)
-    {
-      ansatz10 = uv_xorig[j];
-      ansatz01 = uv_yorig[j];
-      ansatz00 = uv_orig[j];
-      // weighted mass matrix
-      val = ansatz00 * (test00 + tau_m_uptsgradv);
-      sqMatrixM[i][j] += Mult * val;      
-    }// endfor j<N_U
-    
-    for(int j=0; j<N_P; ++j)
-    {
-      // pressure ansatz functions
-      ansatz00 = pq_orig[j];
-      ansatz10 = pq_xorig[j];
-      ansatz01 = pq_yorig[j];
-      
-      val = -ansatz00*test10;
-      val += ansatz10 * tau_m_uptsgradv;
-      reMatrixB1T[i][j] += Mult * val;
-      
-      val = -ansatz00*test01;
-      val += ansatz01*tau_m_uptsgradv;
-      reMatrixB2T[i][j] += Mult * val;
-    }
-  }
-  
-  // velocity(ansatz) pressure (test) blocks
-  for(int i=0; i<N_P; ++i)
-  {
-    test00 = pq_orig[i];
-
-    for(int j=0;j<N_U;j++)
-    {
-      ansatz10 = uv_xorig[j];
-      ansatz01 = uv_yorig[j];
-
-      val = -test00*ansatz10;
-      reMatrixB1[i][j] += Mult * val;
-      
-      val = -test00*ansatz01;
-      reMatrixB2[i][j] += Mult * val;
-    }    
+    double usigmagradv = tau_m * (u1sigma*test10+u2sigma*test01);
+    // right hand side 
+    // standard terms 
+    Rhs1[i] += Mult*c1*(test00 + usigmagradv);
+    Rhs2[i] += Mult*c2*(test00 + usigmagradv);    
   }
 }
 //================================================================================
