65f3f766696ee16a85ed9e9da45070b1c6151a87
ahmed <ahmed@wias-berlin.de>
2017-11-21 10:38:54 +0100

    working commit: smagorinsky model is activated, separate file only for Smagorinsky model.

--- cmake/sources_assembleroutinessaddle.cmake
+++ cmake/sources_assembleroutinessaddle.cmake
@@ -13,4 +13,8 @@ include_directories("${CMAKE_SOURCE_DIR}/include/AssembleRoutinesSaddle")
 list(APPEND ASSEMBLE_SOURCES_2D "${PROJECT_SOURCE_DIR}/src/AssembleRoutinesSaddle/TNSE2DGalerkin.C")
 list(APPEND ASSEMBLE_SOURCES_2D "${PROJECT_SOURCE_DIR}/src/AssembleRoutinesSaddle/NSE2DGalerkin.C")
 
+list(APPEND ASSEMBLE_SOURCES_3D "${PROJECT_SOURCE_DIR}/src/AssembleRoutinesSaddle/TNSE3DSmagorinsky.C")
+list(APPEND ASSEMBLE_SOURCES_3D "${PROJECT_SOURCE_DIR}/src/AssembleRoutinesSaddle/CommonRoutineTNSE3D.C")
+
 list(APPEND PARMOON_SOURCES_2D ${ASSEMBLE_SOURCES_2D})
+list(APPEND PARMOON_SOURCES_3D ${ASSEMBLE_SOURCES_3D})
