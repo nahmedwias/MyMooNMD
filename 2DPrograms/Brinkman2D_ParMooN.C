982b6589a8ecd14e59598fecb45882eeae673e84
Laura Blank <Laura Blank@localhost>
2017-11-27 16:42:01 +0100

    added output of 'Level: 0' on console

--- 2DPrograms/Brinkman2D_ParMooN.C
+++ 2DPrograms/Brinkman2D_ParMooN.C
@@ -10,15 +10,13 @@
 #include <Domain.h>
 #include <Database.h>
 #include <FEDatabase2D.h>
+#include <ParameterDatabase.h>
 #include <LinAlg.h>
 #include <Brinkman2D.h>
 #include <MainUtilities.h>
 #include <LocalAssembling2D.h>
 #include <Example_Brinkman2D.h>
 #include <Chrono.h>
-
-#include <ParameterDatabase.h>
-
 #include <MooNMD_Io.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -34,32 +32,37 @@ int main(int argc, char* argv[])
   //for(refinement_n_initial_steps=1; refinement_n_initial_steps <= 6;++refinement_n_initial_steps)
   //{
 
+  // Start a stopwatch which measures the time spent in different parts of the program
   Chrono timer;
 
-  // declaration of database, you need this in every program
+  // Declaration of the ParMooN Database (ParamDB) and FE2D Database (basis functions etc.), this is obligatory in every program
   TDatabase Database;
   TFEDatabase2D FEDatabase;
 
   ParameterDatabase parmoon_db = ParameterDatabase::parmoon_default_database();
-  std::ifstream fs(argv[1]);
-  parmoon_db.read(fs);
+  std::ifstream fs(argv[1]); // the .dat file is transformed into a stream
+  parmoon_db.read(fs); // all parameters identified (according to read()) in the stream(.dat-file) are saved in parmoon_db
   fs.close();
 
-  /** set variables' value in TDatabase using argv[1] (*.dat file) */
+  // Set each variables' value in TDatabase using argv[1] (*.dat file)
   TDomain Domain(parmoon_db, argv[1]);
 
-  //open OUTFILE, this is where all output is written to (addionally to console)
-
+  // Open OUTFILE, this is where all output is written to (addionally to console)
   Output::set_outfile(parmoon_db["outfile"]);
-
   Output::setVerbosity(parmoon_db["verbosity"]);
 
-  // write all Parameters to the OUTFILE (not to console) for later reference
+  // Write all Parameters to the OUTFILE (not to console) for later reference
   parmoon_db.write(Output::get_outfile());
   Database.WriteParamDB(argv[0]);
 
-  // refine grid
+  // Refine the grid
   size_t n_ref =  Domain.get_n_initial_refinement_steps();
+
+  Output::print("========================================================================================================");
+  Output::print("Level: ", 0);
+
+
+
   //-----------WITHOUT REFINEMENT (for n_initial_refinement_steps: 0)
   // write grid into a Postscript file
   if(parmoon_db["output_write_ps"])
@@ -111,7 +114,9 @@ int main(int argc, char* argv[])
     // write grid into an Postscript file
     if(parmoon_db["output_write_ps"])
       Domain.PS("Domain.ps", It_Finest, 0);
+
     Example_Brinkman2D example(parmoon_db);
+
     timer.restart_and_print("the setup of the domain, database, and example: )");
 
     //=========================================================================
@@ -126,7 +131,6 @@ int main(int argc, char* argv[])
     timer.restart_and_print("solving: ");
     brinkman2d.output();
     timer.restart_and_print("creating the output: ");
-
     //=========================================================================
     Output::print("<<<<< ParMooN Finished: Brinkman2D Main Program >>>>>");
 
@@ -144,8 +148,6 @@ int main(int argc, char* argv[])
     //      Output::print("n_nitsche_boundary: ",TDatabase::ParamDB->n_nitsche_boundary);
     //      Output::print("PkPk_stab: ",TDatabase::ParamDB->PkPk_stab);
     //      Output::print("equal_order_stab_weight_PkPk: ",TDatabase::ParamDB->equal_order_stab_weight_PkPk);
-
-
 
 } //end for(int i = 0; i< n_ref; i++), Compute on all levels
 
