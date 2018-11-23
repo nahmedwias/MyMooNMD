b907b765362a921f78a72b65cf6319a712016c90
Najib Alia <Najib Alia@localhost>
2018-11-23 17:58:11 +0100

    Making optimization classes independent of dimension 2 and 3 as much as possible (working commit)

--- user_projects/boundary_controlled_optimization/NavierStokes_Adjoint.hpp
+++ user_projects/boundary_controlled_optimization/NavierStokes_Adjoint.hpp
@@ -1,5 +1,5 @@
-#ifndef NSE2D_ADJOINT_H
-#define NSE2D_ADJOINT_H
+#ifndef __SYSTEM_NAVIERSTOKES_ADJOINT_H__
+#define __SYSTEM_NAVIERSTOKES_ADJOINT_H__
 
 #include "NavierStokes.h"
 
@@ -7,13 +7,15 @@ template <int d>
 class NavierStokes_Adjoint : public NavierStokes<d>
 {
   public:
+    using FEFunction = typename Template_names<d>::FEFunction;
+    using FEVectFunct = typename Template_names<d>::FEVectFunct;
     
     NavierStokes_Adjoint(const NavierStokes<d>& nse, const ParameterDatabase& param_db);
   
     /// @brief assemble all terms in the matrix and right-hand side which
     /// contain the primal solutions u or p.
-    void assemble(const TFEVectFunct2D& u, const TFEFunction2D& p,
-                  const TFEVectFunct2D& stokes_u, 
+    void assemble(const FEVectFunct& u, const FEFunction& p,
+                  const FEVectFunct& stokes_u, 
                   std::vector<double> cost_functional_weights, 
                   bool restricted_curl_functional);
     
@@ -23,4 +25,4 @@ class NavierStokes_Adjoint : public NavierStokes<d>
     void solve();
 };
 
-#endif // NSE2D_ADJOINT_H
+#endif // __SYSTEM_NAVIERSTOKES_ADJOINT_H__
