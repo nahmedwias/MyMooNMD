ca49b7211e52e120ee2995d6519b56cffc172d82
Baptiste Moreau <Baptiste Moreau@localhost>
2019-03-06 09:48:34 +0100

    - fix small bug to create directory where the snapshots are saved - replace undefined parameter db["snaps_filename"] with db["snaps_basename"] - some cosmetics

--- src/ReducedOrderModels/SnapshotsCollector.C
+++ src/ReducedOrderModels/SnapshotsCollector.C
@@ -1,4 +1,4 @@
-/** ************************************************************************ 
+/** **************************************************************************** 
 *
 * @name       SnapshotsCollector
 * @brief      write/read snapshot data into/from a file
@@ -6,55 +6,68 @@
 * @author     Swetlana Giere & Alfonso Caiazzo
 * @date       08.05.2012 (start of implementaion). Restarted on 15.1.2019
 *
-****************************************************************************/
+*******************************************************************************/
 
 #include <SnapshotsCollector.h>
+#include <sys/stat.h>
 
-/** ***********************************************************************/
-SnapshotsCollector::SnapshotsCollector( const ParameterDatabase& param_db ) :
-  db(ParameterDatabase::get_default_snapshots_database()),snap_count(0)
+/** ***************************************************************************/
+SnapshotsCollector::SnapshotsCollector(const ParameterDatabase& param_db):
+                      db(ParameterDatabase::get_default_snapshots_database()),
+                      snap_count(0)
 {
   this->db.merge(param_db, true);
   snapshot_filename =  this->db["snaps_directory"].get<std::string>() + "/";
-  snapshot_filename += this->db["snaps_filename"].get<std::string>();
+  snapshot_filename += this->db["snaps_basename"].get<std::string>();
+  snapshot_filename += ".snap";
   
   if (db["write_snaps"])
   {
+    // create directory db["snaps_directory"]
+    std::string directory_name = this->db["snaps_directory"].get<std::string>();
+    mkdir(directory_name.c_str(), 0777);
 
-    Output::print<1>( "Filename for storing snapshots: ", snapshot_filename);
+    Output::print<1>("Filename for storing snapshots: ", snapshot_filename);
 
-    this->datastream.open( snapshot_filename.c_str(), ios::out | ios::trunc | ios::in);
-  
+    this->datastream.open(snapshot_filename.c_str(),
+                          ios::out | ios::trunc | ios::in);
+
     if( ! this->datastream.good() )
     {
       ErrThrow("Error: File ", snapshot_filename,
-	       " could not be opened in SnapshotsCollector.\n"
-	       "(No read access to file or file already open)");
+               " could not be opened in SnapshotsCollector.\n"
+               "(No read access to file or file already open)");
     }
-  
-    this->datastream << setprecision( 12 );
+
+    this->datastream << setprecision(12);
   }
 }
 
-/** ***********************************************************************/
+/** ***************************************************************************/
 SnapshotsCollector::~SnapshotsCollector(){
   if( this->datastream.is_open() ) this->datastream.close();
 }
 
-/** ***********************************************************************/
-void SnapshotsCollector::write_data(const BlockVector &solution, size_t time_step )
+/** ***************************************************************************/
+void SnapshotsCollector::write_data(const BlockVector& solution,
+                                    size_t             time_step)
 {
-  if((time_step % (int)db["steps_per_snap"] == 0))
+  if( (time_step % (int)db["steps_per_snap"]) == 0 )
   {
     Output::print<1>("Time step ", time_step, ", writing snapshot n. ",
-		     snap_count+1, " on ", snapshot_filename);
+                     snap_count+1, " on ", snapshot_filename);
     snap_count++;
-    
+
     if( ! this->datastream.is_open() )
-      ErrThrow( "Error: Snapfile is not open." );
+    {
+      ErrThrow("Error: Snapfile is not open.");
+    }
   
-    for(size_t i = 0; i < solution.length(); ++i)
-      this->datastream << solution.get_entries()[ i ] << " ";
+    for( size_t i = 0; i < solution.length(); ++i )
+    {
+      this->datastream << solution.get_entries()[i] << " ";
+    }
+
     this->datastream << endl;
     this->datastream.flush();
   }
