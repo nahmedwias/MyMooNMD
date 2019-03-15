4c7f6ef09055cb3b09bdc2ca5864ab7b6abc91ec
Ulrich Wilbrandt <Ulrich Wilbrandt@localhost>
2019-03-15 17:20:47 +0100

    working commit
    further tnse adjoint stuff

--- user_projects/boundary_controlled_optimization/TimeNavierStokes_Adjoint.hpp
+++ user_projects/boundary_controlled_optimization/TimeNavierStokes_Adjoint.hpp
@@ -12,15 +12,19 @@ class TimeNavierStokes_Adjoint : public TimeNavierStokes<d>
     using DoubleFunction = typename Template_names<d>::DoubleFunction;
     using BoundaryValuesFunction = typename Template_names<d>::BoundaryValuesFunction;
     using Example_TimeNSE = typename Template_names<d>::Example_TimeNSE;
+    using FESpace = typename Template_names<d>::FESpace;
     
-    TimeNavierStokes_Adjoint(const TimeNavierStokes<d>& nse, const ParameterDatabase& param_db);
+    TimeNavierStokes_Adjoint(const TimeNavierStokes<d>& nse,
+                             const ParameterDatabase& param_db);
   
+    /// @brief assemble all terms needed for the first time step (from t=T to 
+    /// t=T-dt).
+    void assemble_initial_time(const FEVectFunct& u, const FEFunction& p);
+    
     /// @brief assemble all terms in the matrix and right-hand side which
     /// contain the primal solutions u or p.
-    void assemble(const FEVectFunct& u, const FEFunction& p,
-                  const FEVectFunct& stokes_u, 
-                  std::vector<double> cost_functional_weights, 
-                  bool restricted_curl_functional);
+    void assemble_matrices_rhs(const FEVectFunct& u,
+                               const FEFunction& p);
     
     /// @brief solve the system.
     /// This is essentially a call to Time_NSE2D::solve, however afterwards scaling
